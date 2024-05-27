53cffc68f5ef03e7802400ee697f6f5ad843a5a2
Hammerbeck <andreas.hammerbeck@digdir.no>
2024-05-27 12:26:06 +0200

    Get request for correspondences

--- src/Altinn.Correspondence.Application/DependencyInjection.cs
+++ src/Altinn.Correspondence.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Altinn.Correspondence.Application.InitializeAttachmentCommand;
+﻿using Altinn.Correspondence.Application.GetCorrespondencesCommand;
+using Altinn.Correspondence.Application.InitializeAttachmentCommand;
 using Altinn.Correspondence.Application.InitializeCorrespondenceCommand;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,5 +10,6 @@ public static class DependencyInjection
     {
         services.AddScoped<InitializeAttachmentCommandHandler>();
         services.AddScoped<InitializeCorrespondenceCommandHandler>();
+        services.AddScoped<GetCorrespondencesCommandHandler>();
     }
 }
