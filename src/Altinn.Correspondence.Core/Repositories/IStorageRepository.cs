1f5b412186f4752fd5ecdeaddd3ac39ca917bc87
Roar Mjelde <roarmj@gmail.com>
2024-05-29 15:16:56 +0200

    Interface for storage operations

---
+++ src/Altinn.Correspondence.Core/Repositories/IStorageRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Mail;
+
+namespace Altinn.Correspondence.Core.Repositories
+{
+    public interface IStorageRepository
+    {
+        Task<string?> UploadAttachment(Guid attachmentId, Stream attachment, CancellationToken cancellationToken);
+        Task<Stream> DownloadAttachment(Guid attachmentId, CancellationToken cancellationToken);
+        Task DeleteAttachment(Guid attachmentId, CancellationToken cancellationToken);
+    }
+}
