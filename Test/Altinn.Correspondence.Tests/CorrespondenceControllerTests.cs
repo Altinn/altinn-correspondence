13cc9966ec519a3fb97ea82775ba55c1e313bef8
Roar Mjelde <roarmj@gmail.com>
2024-06-14 14:29:38 +0200

    Some basic tests

--- Test/Altinn.Correspondence.Tests/CorrespondenceControllerTests.cs
+++ Test/Altinn.Correspondence.Tests/CorrespondenceControllerTests.cs
@@ -106,6 +106,77 @@ public class CorrespondenceControllerTests : IClassFixture<CustomWebApplicationF
         Assert.True(confirmResponse.IsSuccessStatusCode, await confirmResponse.Content.ReadAsStringAsync());
     }
 
+    [Fact]
+    public async Task AttachExistingAttachment_CorrespondencePublished_Fails()
+    {
+        var initializeCorrespondenceResponse = await _client.PostAsJsonAsync("correspondence/api/v1/correspondence", InitializeCorrespondenceFactory.BasicCorrespondence());
+        var correspondence = await initializeCorrespondenceResponse.Content.ReadFromJsonAsync<InitializeCorrespondenceResponseExt>();
+        await UploadAttachment(correspondence?.AttachmentIds.FirstOrDefault());
+        var overview = await _client.GetFromJsonAsync<CorrespondenceOverviewExt>($"correspondence/api/v1/correspondence/{correspondence?.CorrespondenceId}", _responseSerializerOptions);
+        Assert.True(overview?.Status == CorrespondenceStatusExt.Published);
+
+        var attachResponse = await _client.PostAsJsonAsync($"correspondence/api/v1/correspondence/{correspondence?.CorrespondenceId}/attachments", new { AttachmentId = Guid.NewGuid() });
+        Assert.Equal(HttpStatusCode.BadRequest, attachResponse.StatusCode);
+    }
+
+    [Fact]
+    public async Task AttachExistingAttachment_CorrespondenceInitialized_Succeeds()
+    {
+        var initializeCorrespondenceResponse = await _client.PostAsJsonAsync("correspondence/api/v1/correspondence", InitializeCorrespondenceFactory.BasicCorrespondence());
+        var correspondence = await initializeCorrespondenceResponse.Content.ReadFromJsonAsync<InitializeCorrespondenceResponseExt>();
+
+        var attachResponse = await _client.PostAsJsonAsync($"correspondence/api/v1/correspondence/{correspondence?.CorrespondenceId}/attachments", new { AttachmentId = Guid.NewGuid() });
+        Assert.True(attachResponse.IsSuccessStatusCode, await attachResponse.Content.ReadAsStringAsync());
+    }
+
+    [Fact]
+    public async Task AttachExistingAttachment_RemoveIt_NoLongerAttached()
+    {
+        var initializeCorrespondenceResponse = await _client.PostAsJsonAsync("correspondence/api/v1/correspondence", InitializeCorrespondenceFactory.BasicCorrespondence());
+        var correspondence = await initializeCorrespondenceResponse.Content.ReadFromJsonAsync<InitializeCorrespondenceResponseExt>();
+
+        var attachmentId = Guid.NewGuid();
+        await UploadAttachment(attachmentId);
+        var attachResponse = await _client.PostAsJsonAsync($"correspondence/api/v1/correspondence/{correspondence?.CorrespondenceId}/attachments", new { AttachmentId = attachmentId });
+        Assert.True(attachResponse.IsSuccessStatusCode, await attachResponse.Content.ReadAsStringAsync());
+
+        var removeResponse = await _client.DeleteAsync($"correspondence/api/v1/correspondence/{correspondence?.CorrespondenceId}/attachments/{attachmentId}");
+        Assert.True(removeResponse.IsSuccessStatusCode, await removeResponse.Content.ReadAsStringAsync());
+
+        var getAttachmentsResponse = await _client.GetFromJsonAsync<List<Guid>>($"correspondence/api/v1/correspondence/{correspondence?.CorrespondenceId}/attachments");
+        Assert.DoesNotContain(attachmentId, getAttachmentsResponse);
+    }
+
+    [Fact]
+    public async Task AttachExistingAttachment_RemoveNonExisting_FailsWithNotFound()
+    {
+        var initializeCorrespondenceResponse = await _client.PostAsJsonAsync("correspondence/api/v1/correspondence", InitializeCorrespondenceFactory.BasicCorrespondence());
+        var correspondence = await initializeCorrespondenceResponse.Content.ReadFromJsonAsync<InitializeCorrespondenceResponseExt>();
+
+        var attachmentId = Guid.NewGuid();
+        var removeResponse = await _client.DeleteAsync($"correspondence/api/v1/correspondence/{correspondence?.CorrespondenceId}/attachments/{attachmentId}");
+        Assert.Equal(HttpStatusCode.NotFound, removeResponse.StatusCode);
+    }
+
+    [Fact]
+    public async Task AttachExistingAttachment_RemoveTwice_FailsWithNotFoundSecondTime()
+    {
+        var initializeCorrespondenceResponse = await _client.PostAsJsonAsync("correspondence/api/v1/correspondence", InitializeCorrespondenceFactory.BasicCorrespondence());
+        var correspondence = await initializeCorrespondenceResponse.Content.ReadFromJsonAsync<InitializeCorrespondenceResponseExt>();
+
+        var attachmentId = Guid.NewGuid();
+        await UploadAttachment(attachmentId);
+        var attachResponse = await _client.PostAsJsonAsync($"correspondence/api/v1/correspondence/{correspondence?.CorrespondenceId}/attachments", new { AttachmentId = attachmentId });
+        Assert.True(attachResponse.IsSuccessStatusCode, await attachResponse.Content.ReadAsStringAsync());
+
+        var removeResponse1 = await _client.DeleteAsync($"correspondence/api/v1/correspondence/{correspondence?.CorrespondenceId}/attachments/{attachmentId}");
+        Assert.True(removeResponse1.IsSuccessStatusCode, await removeResponse1.Content.ReadAsStringAsync());
+
+        var removeResponse2 = await _client.DeleteAsync($"correspondence/api/v1/correspondence/{correspondence?.CorrespondenceId}/attachments/{attachmentId}");
+        Assert.Equal(HttpStatusCode.NotFound, removeResponse2.StatusCode);
+    }
+
+
     private async Task UploadAttachment(Guid? attachmentId)
     {
         if (attachmentId == null)
