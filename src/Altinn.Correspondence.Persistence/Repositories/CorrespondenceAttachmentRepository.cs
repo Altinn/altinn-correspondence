a1c29d1bff82c9534893858cdf6e7616a79097ca
Hammerbeck <andreas.hammerbeck@digdir.no>
2024-06-28 10:36:11 +0200

    check if correspondence is published

--- src/Altinn.Correspondence.Persistence/Repositories/CorrespondenceAttachmentRepository.cs
+++ src/Altinn.Correspondence.Persistence/Repositories/CorrespondenceAttachmentRepository.cs
@@ -11,6 +11,16 @@ namespace Altinn.Correspondence.Persistence.Repositories
 
         public async Task<Guid?> GetAttachmentIdByCorrespondenceAttachmentId(Guid correspondenceAttachmentId, bool onlyPublished, CancellationToken cancellationToken = default)
         {
+            var correspondencePublished = true;
+
+            if (onlyPublished)
+            {
+                correspondencePublished = await _context.Correspondences
+                    .AnyAsync(c => c.Content != null && c.Content.Attachments.Any(ca => ca.Id == correspondenceAttachmentId && c.Statuses.OrderByDescending(s => s.StatusChanged).First().Status == CorrespondenceStatus.Published));
+
+            }
+            if (!correspondencePublished) return null;
+
             return await _context.CorrespondenceAttachments
                 .Where(ca => ca.Id == correspondenceAttachmentId &&
                 ((onlyPublished && ca.Attachment!.Statuses.OrderByDescending(s => s.StatusChanged).First().Status == AttachmentStatus.Published) || (!onlyPublished))
