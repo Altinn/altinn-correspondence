1b80431efd9f093d5aa35ee16b9ce0328d50791e
Andreas Hammerbeck <Andreas_93@hotmail.com>
2024-07-23 12:48:24 +0200

    mark correspondence as unread (#153)
    * initial functionality
    
    * unRead -> unread
    
    * docs + database changes
    
    * update markedUnread on markasread
    
    * better naming
    
    ---------
    
    Co-authored-by: Hammerbeck <andreas.hammerbeck@digdir.no>

--- src/Altinn.Correspondence.Application/Errors.cs
+++ src/Altinn.Correspondence.Application/Errors.cs
@@ -20,4 +20,5 @@ public static class Errors
     public static Error NoAttachments = new Error(12, "Need atleast one attachments, which must be marked as message body", HttpStatusCode.BadRequest);
     public static Error CorrespondencePurged = new Error(13, "Correspondence has been purged", HttpStatusCode.BadRequest);
     public static Error CorrespondenceAlreadyPurged = new Error(14, "Correspondence has already been purged", HttpStatusCode.BadRequest);
+    public static Error CorrespondenceHasNotBeenRead = new Error(15, "Correspondence has not been read", HttpStatusCode.BadRequest);
 }
---
+++ src/Altinn.Correspondence.Application/UpdateMarkUnread/UpdateMarkAsUnreadHandler.cs
@@ -0,0 +1,44 @@
+using Altinn.Correspondence.Core.Models;
+using Altinn.Correspondence.Core.Models.Enums;
+using Altinn.Correspondence.Core.Repositories;
+using Altinn.Correspondence.Core.Services;
+using Altinn.Correspondence.Core.Services.Enums;
+using OneOf;
+
+namespace Altinn.Correspondence.Application.UpdateMarkAsUnread;
+
+public class UpdateMarkAsUnreadHandler : IHandler<Guid, Guid>
+{
+    private readonly ICorrespondenceRepository _correspondenceRepository;
+    private readonly ICorrespondenceStatusRepository _correspondenceStatusRepository;
+    private readonly IEventBus _eventBus;
+
+    public UpdateMarkAsUnreadHandler(ICorrespondenceRepository correspondenceRepository, ICorrespondenceStatusRepository correspondenceStatusRepository, IEventBus eventBus)
+    {
+        _correspondenceRepository = correspondenceRepository;
+        _correspondenceStatusRepository = correspondenceStatusRepository;
+        _eventBus = eventBus;
+    }
+
+    public async Task<OneOf<Guid, Error>> Process(Guid correspondenceId, CancellationToken cancellationToken)
+    {
+        var correspondence = await _correspondenceRepository.GetCorrespondenceById(correspondenceId, true, false, cancellationToken);
+        if (correspondence == null)
+        {
+            return Errors.CorrespondenceNotFound;
+        }
+
+        var currentStatus = correspondence.Statuses.OrderByDescending(s => s.StatusChanged).FirstOrDefault();
+        if (!correspondence.Statuses.Any(s => s.Status == CorrespondenceStatus.Read))
+        {
+            return Errors.CorrespondenceHasNotBeenRead;
+        }
+        if (currentStatus?.Status == CorrespondenceStatus.PurgedByRecipient || currentStatus?.Status == CorrespondenceStatus.PurgedByAltinn)
+        {
+            return Errors.CorrespondencePurged;
+        }
+
+        await _correspondenceRepository.UpdateMarkedUnread(correspondenceId, true, cancellationToken);
+        return correspondenceId;
+    }
+}
