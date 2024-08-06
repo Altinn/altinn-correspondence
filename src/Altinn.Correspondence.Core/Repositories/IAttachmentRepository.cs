1eddd7e49798570dbf40dcfe4b10f41874aba2d4
CelineTrammi <celinetram@gmail.com>
2024-08-06 10:32:48 +0200

    InitializeAttachment returns AttachmentEntity instead of Guid

--- src/Altinn.Correspondence.Core/Repositories/IAttachmentRepository.cs
+++ src/Altinn.Correspondence.Core/Repositories/IAttachmentRepository.cs
@@ -5,7 +5,7 @@ namespace Altinn.Correspondence.Core.Repositories
 {
     public interface IAttachmentRepository
     {
-        Task<Guid> InitializeAttachment(AttachmentEntity attachment, CancellationToken cancellationToken);
+        Task<AttachmentEntity> InitializeAttachment(AttachmentEntity attachment, CancellationToken cancellationToken);
         Task<List<Guid>> InitializeMultipleAttachments(List<AttachmentEntity> attachments, CancellationToken cancellationToken);
         Task<AttachmentEntity?> GetAttachmentByUrl(string url, CancellationToken cancellationToken);
         Task<AttachmentEntity?> GetAttachmentById(Guid attachmentId, bool includeStatus = false, CancellationToken cancellationToken = default);
