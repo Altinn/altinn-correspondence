680287792d85110bd9dd6518d7c98970fc851154
Hammerbeck <andreas.hammerbeck@digdir.no>
2024-08-14 10:18:55 +0200

    set max attempt to 25

--- .github/tools/revisionVerifier.sh
+++ .github/tools/revisionVerifier.sh
@@ -31,7 +31,7 @@ verify_revision() {
   if [[ $health_state == "Healthy" && ($running_state == "Running" || $running_state == "RunningAtMaxScale") ]]; then
     return 0  # OK!
   else
-    if [ $running_state == "Failed" ]; then
+    if [[ $running_state == "Failed"]]; then
       echo "Revision $revision_name failed. Exiting script."
       exit 1
     fi 
@@ -50,5 +50,8 @@ while true; do
     echo "Attempt $attempt: Waiting for revision $revision_name ..."
     sleep 10 # Sleep for 10 seconds
     attempt=$((attempt+1))
+    if [[ $attempt -gt 25 ]]; then
+      echo "Revision $revision_name did not start in time. Exiting script."
+      exit 1
   fi
 done
