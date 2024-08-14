f892282bdc92f817ce4b81805e91cec4ee819724
Roar Mjelde <roarmj@gmail.com>
2024-08-14 13:19:50 +0200

    Set permissions after schema has been created

--- src/Altinn.Correspondence.Persistence/Migrations/20240814104030_Initial_DB_Setup.cs
+++ src/Altinn.Correspondence.Persistence/Migrations/20240814104030_Initial_DB_Setup.cs
@@ -312,26 +312,6 @@ namespace Altinn.Correspondence.Persistence.Migrations
                 schema: "correspondence",
                 table: "ExternalReferences",
                 column: "CorrespondenceId");
-
-            migrationBuilder.Sql(
-                @$"
-                    DO $do$
-                    DECLARE
-                        role_count INTEGER;
-                    BEGIN
-                        SELECT COUNT(*) INTO role_count FROM pg_roles WHERE rolname = 'azure_pg_admin';
-
-                        IF role_count > 0 THEN
-                            GRANT ALL ON SCHEMA correspondence TO azure_pg_admin;
-                            GRANT ALL ON ALL TABLES IN SCHEMA correspondence TO azure_pg_admin;
-                            GRANT ALL ON ALL SEQUENCES IN SCHEMA correspondence TO azure_pg_admin;
-                            ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence GRANT ALL ON TABLES TO azure_pg_admin;
-                            ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence GRANT ALL ON SEQUENCES TO azure_pg_admin;
-                        END IF;
-                    END
-                    $do$;
-                "
-            );
         }
 
         /// <inheritdoc />
--- src/Altinn.Correspondence.Persistence/Migrations/20240814104247_Hangfire_Setup.cs
+++ src/Altinn.Correspondence.Persistence/Migrations/20240814104247_Hangfire_Setup.cs
@@ -10,13 +10,30 @@ namespace Altinn.Correspondence.Persistence.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql(
+            @$"
+                DO $do$
+                DECLARE
+                    role_count INTEGER;
+                BEGIN
+                    SELECT COUNT(*) INTO role_count FROM pg_roles WHERE rolname = 'azure_pg_admin';
 
+                    IF role_count > 0 THEN
+                        CREATE SCHEMA hangfire;
+                        GRANT ALL ON SCHEMA hangfire TO azure_pg_admin;
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA hangfire GRANT ALL ON TABLES TO azure_pg_admin;
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA hangfire GRANT ALL ON SEQUENCES TO azure_pg_admin;
+                    END IF;
+                END
+                $do$;
+                "
+            );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropSchema("hangfire");
         }
     }
 }
