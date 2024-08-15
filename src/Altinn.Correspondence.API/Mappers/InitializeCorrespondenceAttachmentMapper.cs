bd82802fa81ea05a338f22eb11bd35ee0b8595e7
Roar Mjelde <roarmj@gmail.com>
2024-08-15 14:12:34 +0200

    Sender is associated with the correspondence itself, not the correspondence attachment. Hence it should not be exposed as a property of a correspondence attachment in the API.

--- src/Altinn.Correspondence.API/Mappers/InitializeCorrespondenceAttachmentMapper.cs
+++ src/Altinn.Correspondence.API/Mappers/InitializeCorrespondenceAttachmentMapper.cs
@@ -6,16 +6,7 @@ namespace Altinn.Correspondence.Mappers;
 
 internal static class InitializeCorrespondenceAttachmentMapper
 {
-    internal static List<CorrespondenceAttachmentEntity> MapListToEntities(List<InitializeCorrespondenceAttachmentExt> initializeAttachmentExt, string resourceId)
-    {
-        var attachments = new List<CorrespondenceAttachmentEntity>();
-        foreach (var attachment in initializeAttachmentExt)
-        {
-            attachments.Add(MapToEntity(attachment, resourceId));
-        }
-        return attachments;
-    }
-    internal static CorrespondenceAttachmentEntity MapToEntity(InitializeCorrespondenceAttachmentExt initializeAttachmentExt, string resourceId)
+    internal static CorrespondenceAttachmentEntity MapToEntity(InitializeCorrespondenceAttachmentExt initializeAttachmentExt, string resourceId, string sender)
     {
         return new CorrespondenceAttachmentEntity
         {
@@ -28,7 +19,7 @@ internal static class InitializeCorrespondenceAttachmentMapper
                 Name = initializeAttachmentExt.Name,
                 RestrictionName = initializeAttachmentExt.RestrictionName,
                 ResourceId = resourceId,
-                Sender = initializeAttachmentExt.Sender,
+                Sender = sender,
                 SendersReference = initializeAttachmentExt.SendersReference,
                 DataType = initializeAttachmentExt.DataType,
                 Checksum = initializeAttachmentExt.Checksum,
