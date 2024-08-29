a5d0aa10c70540c77914e0ca8abaf3fbf29c6ac8
CelineTrammi <celinetram@gmail.com>
2024-08-29 10:14:22 +0200

    create base class for Attachment

---
+++ src/Altinn.Correspondence.API/Models/BaseAttachmentExt.cs
@@ -0,0 +1,61 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Altinn.Correspondence.API.Models
+{
+    public class BaseAttachmentExt
+    {
+        /// <summary>
+        /// The name of the attachment file.
+        /// </summary>
+        [JsonPropertyName("fileName")]
+        [StringLength(255, MinimumLength = 0)]
+        public string? FileName { get; set; }
+
+        /// <summary>
+        /// A logical name on the attachment.
+        /// </summary>
+        [JsonPropertyName("name")]
+        [StringLength(255, MinimumLength = 1)]
+        [Required]
+        public required string Name { get; set; }
+
+        /// <summary>
+        /// The name of the Restriction Policy restricting access to this element
+        /// </summary>
+        /// <remarks>
+        /// An empty value indicates no restriction above the ones governing the correspondence referencing this attachment
+        /// </remarks>
+        [JsonPropertyName("restrictionName")]
+        [Required]
+        public string RestrictionName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// A value indicating whether the attachment is encrypted or not.
+        /// </summary>
+        [JsonPropertyName("isEncrypted")]
+        public bool IsEncrypted { get; set; }
+
+        /// <summary>
+        /// MD5 checksum for file data.
+        /// </summary>
+        [JsonPropertyName("checksum")]
+        [MD5Checksum]
+        public string? Checksum { get; set; } = string.Empty;
+
+        /// <summary>
+        /// A reference value given to the attachment by the creator.
+        /// </summary>
+        [JsonPropertyName("sendersReference")]
+        [StringLength(4096, MinimumLength = 1)]
+        [Required]
+        public required string SendersReference { get; set; }
+
+        /// <summary>
+        /// The attachment data type in MIME format
+        /// </summary>
+        [JsonPropertyName("dataType")]
+        [Required]
+        public required string DataType { get; set; }
+    }
+}
--- src/Altinn.Correspondence.API/Models/InitializeCorrespondenceAttachmentExt.cs
+++ src/Altinn.Correspondence.API/Models/InitializeCorrespondenceAttachmentExt.cs
@@ -7,66 +7,13 @@ namespace Altinn.Correspondence.API.Models
     /// <summary>
     /// Represents an attachment to a specific correspondence as part of Initialize Correpondence Operation
     /// </summary>
-    public class InitializeCorrespondenceAttachmentExt
+    public class InitializeCorrespondenceAttachmentExt : BaseAttachmentExt
     {
         /// <summary>
         /// A unique id for the correspondence attachment.
         /// 
         [JsonPropertyName("id")]
         public Guid Id { get; set; }
-
-        /// <summary>
-        /// The name of the attachment file.
-        /// </summary>
-        [JsonPropertyName("fileName")]
-        [StringLength(255, MinimumLength = 0)]
-        public string? FileName { get; set; }
-
-        /// <summary>
-        /// A logical name on the attachment.
-        /// </summary>
-        [JsonPropertyName("name")]
-        [StringLength(255, MinimumLength = 1)]
-        [Required]
-        public required string Name { get; set; }
-
-        /// <summary>
-        /// A value indicating whether the attachment is encrypted or not.
-        /// </summary>
-        [JsonPropertyName("isEncrypted")]
-        public bool IsEncrypted { get; set; }
-
-        /// <summary>
-        /// MD5 checksum for file data.
-        /// </summary>
-        [JsonPropertyName("checksum")]
-        [MD5Checksum]
-        public string? Checksum { get; set; } = string.Empty;
-
-        /// <summary>
-        /// A reference value given to the attachment by the creator.
-        /// </summary>
-        [JsonPropertyName("sendersReference")]
-        [StringLength(4096, MinimumLength = 1)]
-        [Required]
-        public required string SendersReference { get; set; }
-
-        /// <summary>
-        /// The attachment data type in MIME format
-        /// </summary>
-        [JsonPropertyName("dataType")]
-        [Required]
-        public required string DataType { get; set; }
-
-        /// <summary>
-        /// The name of the Restriction Policy restricting access to this element
-        /// </summary>
-        /// <remarks>
-        /// An empty value indicates no restriction above the ones governing the correspondence referencing this attachment
-        /// </remarks>
-        [JsonPropertyName("restrictionName")]
-        [Required]
-        public string RestrictionName { get; set; } = string.Empty;
 
         /// <summary>
         /// Specifies the location type of the attachment data
