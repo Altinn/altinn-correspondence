ab63d3229bcf55e22174348361fdda36c92742de
CelineTrammi <celinetram@gmail.com>
2024-09-23 15:55:58 +0200

    rename DownloadCorrespondence to DownloadCorrespondenceAttachment

--- src/Altinn.Correspondence.Application/DownloadCorrespondenceAttachment/DownloadCorrespondenceAttachmentHandler.cs
+++ src/Altinn.Correspondence.Application/DownloadCorrespondenceAttachment/DownloadCorrespondenceAttachmentHandler.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using Altinn.Correspondence.Application.Helpers;
+﻿using Altinn.Correspondence.Application.Helpers;
 using Altinn.Correspondence.Core.Models.Enums;
 using Altinn.Correspondence.Core.Repositories;
 using OneOf;
