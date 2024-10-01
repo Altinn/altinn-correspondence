191e83a2ac49de2b820519ef0dfabccd71466254
Roar Mjelde <roarmj@gmail.com>
2024-10-01 13:20:34 +0200

    Integrated with Dialogportens activity log

---
+++ src/Altinn.Correspondence.Integrations/Dialogporten/CreateDialogActivityRequestMapper.cs
@@ -0,0 +1,41 @@
+﻿using Altinn.Correspondence.Core.Models.Entities;
+using Altinn.Correspondence.Core.Services.Enums;
+using UUIDNext;
+
+namespace Altinn.Correspondence.Integrations.Dialogporten
+{
+    internal class CreateDialogActivityRequestMapper
+    {
+        internal static CreateDialogActivityRequest CreateDialogActivityRequest(CorrespondenceEntity correspondence, DialogportenActorType actorType, string description, string? extendedType, ActivityType activityType = ActivityType.Information)
+        {
+            var dialogActivityId = Uuid.NewDatabaseFriendly(Database.PostgreSql).ToString(); // Dialogporten requires time-stamped GUIDs, not supported natively until .NET 9.0
+            var urnActorId = actorType switch
+            {
+                DialogportenActorType.ServiceOwner => null,
+                DialogportenActorType.Sender => correspondence.GetSenderUrn(),
+                DialogportenActorType.Recipient => correspondence.GetRecipientUrn(),
+                _ => throw new NotImplementedException()
+            };
+            return new CreateDialogActivityRequest()
+            {
+                Id = dialogActivityId,
+                CreatedAt = DateTime.UtcNow,
+                Description = new List<ActivityDescription>()
+                {
+                    new ActivityDescription()
+                    {
+                        LanguageCode = "nb",
+                        Value = description
+                    }
+                },
+                ExtendedType = extendedType,
+                PerformedBy = new ActivityPerformedBy()
+                {
+                    ActorType = actorType == DialogportenActorType.ServiceOwner ? "ServiceOwner" : "PartyRepresentative",
+                    ActorId = urnActorId
+                },
+                Type = activityType
+            };
+        }
+    }
+}
---
+++ src/Altinn.Correspondence.Integrations/Dialogporten/FormatHelper.cs
@@ -0,0 +1,60 @@
+﻿using Altinn.Correspondence.Core.Models.Entities;
+using Altinn.Correspondence.Core.Models.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Altinn.Correspondence.Integrations.Dialogporten
+{
+    public static class FormatHelper
+    {
+        public static string GetSenderUrn(this CorrespondenceEntity correspondence)
+        {
+            var urn = GetUrn(correspondence.Sender);
+            if (urn is null)
+            {
+                throw new ArgumentException("Correspondence had invalid recipient");
+            }
+            return urn;
+        }
+
+        public static string GetRecipientUrn(this CorrespondenceEntity correspondence)
+        {
+            var urn = GetUrn(correspondence.Recipient);
+            if (urn is null)
+            {
+                throw new ArgumentException("Correspondence had invalid recipient");
+            }
+            return urn;
+        }
+
+        private const string OrgNoPrefix = "urn:altinn:organization:identifier-no";
+        private const string SsnPrefix = "urn:altinn:person:identifier-no";
+
+        private static string? GetUrn(string input)
+        {
+            var organizationWithoutPrefixFormat = new Regex(@"^\d{9}$");
+            var organizationWithPrefixFormat = new Regex(@"^\d{4}:\d{9}$");
+            var personFormat = new Regex(@"^\d{11}$");
+            if (organizationWithoutPrefixFormat.IsMatch(input))
+            {
+                return $"{OrgNoPrefix}:{input}";
+            }
+            else if (organizationWithPrefixFormat.IsMatch(input))
+            {
+                return $"{OrgNoPrefix}:{input.Substring(5)}";
+            }
+            else if (personFormat.IsMatch(input))
+            {
+                return $"{SsnPrefix}:{input}";
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
