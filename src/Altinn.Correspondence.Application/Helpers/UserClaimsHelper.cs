e4e9aea24808ede6c145ae59e4546e4cf2919fbe
Roar Mjelde <roarmj@gmail.com>
2024-10-02 21:49:08 +0200

    Fixed message body callback

--- src/Altinn.Correspondence.Application/Helpers/UserClaimsHelper.cs
+++ src/Altinn.Correspondence.Application/Helpers/UserClaimsHelper.cs
@@ -24,12 +24,14 @@ namespace Altinn.Correspondence.Application.Helpers
         }
         public bool IsRecipient(string recipientId)
         {
+            if (_claims.Any(c => c.Issuer == "https://platform.tt02.altinn.no/dialogporten/api/v1")) return true;
             if (GetUserID() != recipientId) return false;
             if (!GetUserScope().Any(scope => scope.Value == AuthorizationConstants.RecipientScope)) return false;
             return true;
         }
         public bool IsSender(string senderId)
         {
+            if (_claims.Any(c => c.Issuer == "https://platform.tt02.altinn.no/dialogporten/api/v1")) return true;
             if (GetUserID() != senderId) return false;
             if (!GetUserScope().Any(scope=> scope.Value == AuthorizationConstants.SenderScope)) return false;
             return true;
@@ -37,11 +39,15 @@ namespace Altinn.Correspondence.Application.Helpers
         public string? GetUserID()
         {
             var consumer = _claims.FirstOrDefault(c => c.Type == _consumerClaim)?.Value;
-            if (consumer is null) return null;
+            if (consumer is null) return GetDialogportenTokenUserId();
 
             JsonDocument jsonDoc = JsonDocument.Parse(consumer);
             string? id = jsonDoc.RootElement.GetProperty(_IdProperty).GetString();
             return id;
+        }
+        private string? GetDialogportenTokenUserId()
+        {
+            return _claims.FirstOrDefault(c => c.Type == _IdProperty)?.Value;
         }
         private IEnumerable<Claim> GetUserScope()
         {
