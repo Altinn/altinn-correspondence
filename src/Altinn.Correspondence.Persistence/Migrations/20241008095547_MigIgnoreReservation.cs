6398bc713d8c75076d47695ca991fed678da54e4
Martin Todorov <>
2024-10-08 11:58:20 +0200

    Endringer etter migrering av db

---
+++ src/Altinn.Correspondence.Persistence/Migrations/20241008095547_MigIgnoreReservation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Correspondence.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigIgnoreReservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsReservable",
+                schema: "correspondence",
+                table: "Correspondences",
+                newName: "IgnoreReservation");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IgnoreReservation",
+                schema: "correspondence",
+                table: "Correspondences",
+                newName: "IsReservable");
+        }
+    }
+}
