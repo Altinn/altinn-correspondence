fabc12798fe47f5581a52fdf5577a1c70d9027d7
Roar Mjelde <roarmj@gmail.com>
2024-10-10 10:46:28 +0200

    Fix log level

--- src/Altinn.Correspondence.API/Program.cs
+++ src/Altinn.Correspondence.API/Program.cs
@@ -8,11 +8,14 @@ using Altinn.Correspondence.Integrations.Hangfire;
 using Altinn.Correspondence.Persistence;
 using Azure.Identity;
 using Hangfire;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.IdentityModel.Tokens;
 using Npgsql;
 using System.Text.Json.Serialization;
@@ -148,7 +151,52 @@ static void ConfigureServices(IServiceCollection services, IConfiguration config
                 ValidateLifetime = !hostEnvironment.IsDevelopment(),
                 ClockSkew = TimeSpan.Zero
             };
-        });
+        })
+        .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+        {
+            options.Cookie.Name = "session";
+            options.Cookie.SameSite = SameSiteMode.None;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            options.Cookie.IsEssential = true;
+        })
+         .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+         {
+             options.NonceCookie.SecurePolicy = CookieSecurePolicy.Always;
+             options.CorrelationCookie.SecurePolicy = CookieSecurePolicy.Always;
+             options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+             options.GetClaimsFromUserInfoEndpoint = true;
+             options.AuthenticationMethod = OpenIdConnectRedirectBehavior.RedirectGet;
+             options.ResponseMode = OpenIdConnectResponseMode.FormPost;
+             options.Authority = "https://test.idporten.no/";
+             options.ClientId = configuration["Idporten:ClientId"];
+             options.ClientSecret = configuration["Idporten:Secret"];
+             options.ResponseType = OpenIdConnectResponseType.Code;
+             options.UsePkce = true;
+             options.CallbackPath = configuration["Idporten:CallbackPath"];
+             options.SaveTokens = true;
+             options.GetClaimsFromUserInfoEndpoint = false;
+             options.Scope.Add("openid");
+             options.Scope.Add("profile");
+
+             options.Events = new OpenIdConnectEvents
+             {
+                 OnRedirectToIdentityProviderForSignOut = context =>
+                 {
+                     context.Response.Redirect(configuration["Idporten:RedirectOnSignOut"]!);
+                     context.HandleResponse();
+
+                     return Task.CompletedTask;
+                 },
+
+                 OnRemoteFailure = context =>
+                 {
+                     context.Response.Redirect("/error");
+                     context.HandleResponse();
+                     return Task.FromResult(0);
+                 },
+             };
+         });
+    ;
     services.AddTransient<IAuthorizationHandler, ScopeAccessHandler>();
     services.AddAuthorization(options =>
     {
@@ -163,6 +211,10 @@ static void ConfigureServices(IServiceCollection services, IConfiguration config
         });
         options.AddPolicy(AuthorizationConstants.Migrate, policy => policy.AddRequirements(new ScopeAccessRequirement(AuthorizationConstants.MigrateScope)).AddAuthenticationSchemes(AuthorizationConstants.MaskinportenScheme));
         options.AddPolicy(AuthorizationConstants.NotificationCheck, policy => policy.AddRequirements(new ScopeAccessRequirement(AuthorizationConstants.NotificationCheckScope)).AddAuthenticationSchemes(AuthorizationConstants.MaskinportenScheme));
+        options.AddPolicy("DownloadAttachment", policy =>
+        {
+            policy.RequireScopesUnlessDialogporten(config, AuthorizationConstants.SenderScope, AuthorizationConstants.RecipientScope).AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme, AuthorizationConstants.DialogportenScheme);
+        });
     });
     services.AddEndpointsApiExplorer();
     services.AddSwaggerGen();
