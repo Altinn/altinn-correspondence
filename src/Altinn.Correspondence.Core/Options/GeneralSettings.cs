067b2daf50ae3a32dd550141ac258f2652a4221d
Roar Mjelde <roarmj@gmail.com>
2024-10-22 11:45:55 +0200

    Nitpick

--- src/Altinn.Correspondence.Core/Options/GeneralSettings.cs
+++ src/Altinn.Correspondence.Core/Options/GeneralSettings.cs
@@ -2,7 +2,7 @@ namespace Altinn.Correspondence.Core.Options;
 
 public class GeneralSettings
 {
-    public string SlackUrl { get; set; } = "";
+    public string SlackUrl { get; set; } = string.Empty;
 
     public string CorrespondenceBaseUrl { get; set; } = string.Empty;
 }
