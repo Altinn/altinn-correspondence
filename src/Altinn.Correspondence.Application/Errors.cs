4b3aca8706105e2e97db622c34c008fa05bf1e9b
CelineTrammi <celinetram@gmail.com>
2024-10-25 10:24:26 +0200

    some more information in the error message

--- src/Altinn.Correspondence.Application/Errors.cs
+++ src/Altinn.Correspondence.Application/Errors.cs
@@ -48,5 +48,5 @@ public static class Errors
     public static Error LegacyNotAccessToOwner(int partyId) { return new Error(40, $"User does not have access to party with partyId {partyId}", HttpStatusCode.Unauthorized); }
     public static Error CorrespondenceNotConfirmed = new Error(41, "Cannot archive or delete a correspondence which has not been confirmed when confirmation is required", HttpStatusCode.BadRequest);
     public static Error DueDateRequired = new Error(42, "DueDateTime is required when confirmation is needed", HttpStatusCode.BadRequest);
-    public static Error MissingContent = new Error(43, "Content must be provided for the correspondence", HttpStatusCode.BadRequest);
+    public static Error MissingContent = new Error(43, "The Content field must be provided for the correspondence", HttpStatusCode.BadRequest);
 }
