8c4323eaec4197fd193e916837edede08aeb70e0
CelineTrammi <celinetram@gmail.com>
2024-10-25 14:57:42 +0200

    update database with correct language code

---
+++ src/Altinn.Correspondence.Persistence/Migrations/20241025123814_Update_Notication_Template_LanguageCode.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Correspondence.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Notication_Template_LanguageCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "correspondence",
+                table: "NotificationTemplates",
+                keyColumn: "Language",
+                keyValue: "no",
+                column: "Language",
+                value: "nb"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "correspondence",
+                table: "NotificationTemplates",
+                keyColumn: "Language",
+                keyValue: "nb",
+                column: "Language",
+                value: "no"
+            );
+        }
+    }
+}
