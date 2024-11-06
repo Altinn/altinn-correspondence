f45ab98c0923a7b730165b68d765a6b8025e6355
CelineTrammi <celinetram@gmail.com>
2024-11-06 14:47:13 +0100

    add in dependencyInjection also

--- src/Altinn.Correspondence.Application/DependencyInjection.cs
+++ src/Altinn.Correspondence.Application/DependencyInjection.cs
@@ -53,6 +53,7 @@ public static class DependencyInjection
         services.AddScoped<UploadHelper>();
         services.AddScoped<UserClaimsHelper>();
         services.AddScoped<InitializeCorrespondenceHelper>();
+        services.AddScoped<UpdateCorrespondenceStatusHelper>();
 
         // Legacy
         services.AddScoped<LegacyGetCorrespondencesHandler>();
