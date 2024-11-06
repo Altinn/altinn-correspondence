e0de97faf15e62a080d4754d11e1a0441c4e962a
CelineTrammi <61122289+CelineTrammi@users.noreply.github.com>
2024-11-06 12:39:35 +0100

    fix: use correct lookupParty function, change onbehalfofpartyId to int (#444)
    * fix: use correct lookup function, onbehalfofpartyId is int
    
    * remove onBehalfOfPartyId and implement minimumAuthLevel
    
    * change userClaimsHelper to readonly and simplify partyId retrieval

--- src/Altinn.Correspondence.Application/GetCorrespondenceHistory/LegacyGetCorrespondenceHistoryRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace Altinn.Correspondence.Application.GetCorrespondenceHistory;
-
-public class LegacyGetCorrespondenceHistoryRequest
-{
-    public required string OnBehalfOfPartyId { get; set; }
-    public required Guid CorrespondenceId { get; set; }
-}
