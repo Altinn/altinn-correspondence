259f426ef1b2af6d98e79984b1cdc9fcefafdb77
Hammerbeck <andreas.hammerbeck@digdir.no>
2024-11-18 09:07:58 +0100

    add jsonPropertyNames where missing

--- src/Altinn.Correspondence.API/Models/NotificationRecipientExt.cs
+++ src/Altinn.Correspondence.API/Models/NotificationRecipientExt.cs
@@ -1,4 +1,6 @@
 
+using System.Text.Json.Serialization;
+
 namespace Altinn.Correspondence.API.Models;
 
 /// <summary>
@@ -12,25 +14,30 @@ public class NotificationRecipientExt
     /// <summary>
     /// the email address of the recipient
     /// </summary>
+    [JsonPropertyName("emailAddress")]
     public string? EmailAddress { get; set; }
 
     /// <summary>
     /// the mobileNumber of the recipient
     /// </summary>
+    [JsonPropertyName("mobileNumber")]
     public string? MobileNumber { get; set; }
 
     /// <summary>
     /// the organization number of the recipient
     /// </summary>
+    [JsonPropertyName("organizationNumber")]
     public string? OrganizationNumber { get; set; }
 
     /// <summary>
     /// The SSN of the recipient
     /// </summary>
+    [JsonPropertyName("nationalIdentityNumber")]
     public string? NationalIdentityNumber { get; set; }
 
     /// <summary>
     /// Boolean indicating if the recipient is reserved
     /// </summary>
+    [JsonPropertyName("isReserved")]
     public bool? IsReserved { get; set; }
 }
