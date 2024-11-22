34107a8beea09a39310efb9f80cafe1bcf32708b
CelineTrammi <celinetram@gmail.com>
2024-11-22 12:52:17 +0100

    separate user and notification fields

--- src/Altinn.Correspondence.Application/GetCorrespondenceHistory/LegacyGetCorrespondenceHistoryResponse.cs
+++ src/Altinn.Correspondence.Application/GetCorrespondenceHistory/LegacyGetCorrespondenceHistoryResponse.cs
@@ -8,9 +8,19 @@ public class LegacyGetCorrespondenceHistoryResponse
     public DateTimeOffset? StatusChanged { get; set; }
     public string StatusText { get; set; }
     public LegacyUser User { get; set; }
+    public LegacyNotification? Notification { get; set; }
 }
 public class LegacyUser
 {
     public int? PartyId { get; set; }
-    public Recipient Recipient { get; set; }
+    public string? OrganizationNumber { get; set; }
+    public string? NationalIdentityNumber { get; set; }
+    public string? Name { get; set; }
+}
+public class LegacyNotification
+{
+    public string? EmailAddress { get; set; }
+    public string? MobileNumber { get; set; }
+    public string? OrganizationNumber { get; set; }
+    public string? NationalIdentityNumber { get; set; }
 }
