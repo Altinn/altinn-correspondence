209333ca68d931ebd0981b2d7ea84fb6cc251291
CelineTrammi <celinetram@gmail.com>
2024-12-09 14:27:30 +0100

    add more details for recipientLookUpStatus

--- src/Altinn.Correspondence.Core/Models/Notifications/RecipientLookupResult.cs
+++ src/Altinn.Correspondence.Core/Models/Notifications/RecipientLookupResult.cs
@@ -12,11 +12,23 @@ public class RecipientLookupResult
     public List<string>? MissingContact { get; set; }
 }
 
+/// <summary>
+/// Enum describing the success rate for recipient lookup
+/// </summary>
 public enum RecipientLookupStatus
 {
+    /// <summary>
+    /// The recipient lookup was successful for all recipients
+    /// </summary>
     Success,
 
+    /// <summary>
+    /// The recipient lookup was successful for some recipients
+    /// </summary>
     PartialSuccess,
 
+    /// <summary>
+    /// The recipient lookup failed for all recipients
+    /// </summary>
     Failed
 }
