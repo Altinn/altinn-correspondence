9e6c94a8b295051987547887d3abab47ce4e429f
Andreas Hammerbeck <Andreas_93@hotmail.com>
2025-01-09 12:41:48 +0100

    fix subunits for legacy search (#580)
    * fix subunits for search
    
    * cleanup
    
    * add a max depth to sub units in case of malicious data
    
    ---------
    
    Co-authored-by: Hammerbeck <andreas.hammerbeck@digdir.no>

--- src/Altinn.Correspondence.Application/GetCorespondences/LegacyGetCorrespondencesHandler.cs
+++ src/Altinn.Correspondence.Application/GetCorespondences/LegacyGetCorrespondencesHandler.cs
@@ -46,7 +46,7 @@ public class LegacyGetCorrespondencesHandler(
         if (request.InstanceOwnerPartyIdList != null && request.InstanceOwnerPartyIdList.Length > 0)
         {
             var authorizedParties = await altinnAccessManagementService.GetAuthorizedParties(userParty, cancellationToken);
-            var authorizedPartiesDict = authorizedParties.ToDictionary(c => c.PartyId);
+            var authorizedPartiesDict = authorizedParties.ToDictionary(p => p.PartyId, p => p);
             foreach (int instanceOwnerPartyId in request.InstanceOwnerPartyIdList)
             {
                 if (!authorizedPartiesDict.TryGetValue(instanceOwnerPartyId, out var mappedInstanceOwner))
