f26a9fa84c4ee47be87ece8591eb1ad8a7227a68
Roar Mjelde <roarmj@gmail.com>
2025-01-14 11:20:51 +0100

    Cosmetic fix to sql scripts

--- Altinn.Correspondence.LoadTests.DatabasePopulater/populate_test_database.sql
+++ Altinn.Correspondence.LoadTests.DatabasePopulater/populate_test_database.sql
@@ -5,15 +5,8 @@ DECLARE
     batches int;
     remaining_records int;
     i int;
-BEGIN
-    -- Disable triggers and indexes temporarily for better performance
-    -- ALTER TABLE "correspondence"."Correspondences" DISABLE TRIGGER ALL;
-    -- ALTER TABLE "correspondence"."CorrespondenceStatuses" DISABLE TRIGGER ALL;
-    -- ALTER TABLE "correspondence"."CorrespondenceContents" DISABLE TRIGGER ALL;
-    -- ALTER TABLE "correspondence"."CorrespondenceReplyOptions" DISABLE TRIGGER ALL;
-    -- ALTER TABLE "correspondence"."CorrespondenceNotifications" DISABLE TRIGGER ALL;
-                
-    -- SET session_replication_role = replica; // Must be set in Azure Resource call
+BEGIN        
+    -- SET session_replication_role = replica; // Must be set in Azure DbServer Server Parameters        
     batches := total_records / 1000;
     remaining_records := total_records % 1000;
 
@@ -25,13 +18,6 @@ BEGIN
     IF remaining_records > 0 THEN
         PERFORM generate_test_data(remaining_records);
     END IF;
-
     -- SET session_replication_role = DEFAULT;
-    -- Re-enable triggers and rebuild indexes
-    -- ALTER TABLE "correspondence"."Correspondences" ENABLE TRIGGER ALL;
-    -- ALTER TABLE "correspondence"."CorrespondenceStatuses" ENABLE TRIGGER ALL;
-    -- ALTER TABLE "correspondence"."CorrespondenceContents" ENABLE TRIGGER ALL;
-    -- ALTER TABLE "correspondence"."CorrespondenceReplyOptions" ENABLE TRIGGER ALL;
-    -- ALTER TABLE "correspondence"."CorrespondenceNotifications" ENABLE TRIGGER ALL;
 END;
 $$ LANGUAGE plpgsql;
