1ba76ed66b62386bed80807a0a219b8d8a7bbc5a
Martin S. Todorov <martin.solberg.todorov@digdir.no>
2025-01-20 14:23:14 +0100

    Changed to standard mime type
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/Altinn.Correspondence.Common/Helpers/FileConstants.cs
+++ src/Altinn.Correspondence.Common/Helpers/FileConstants.cs
@@ -4,21 +4,21 @@ namespace Altinn.Correspondence.Common.Helpers
     {
         public static readonly IReadOnlyDictionary<string, string> MimeTypes = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
         {
-            { ".doc", "application_msword" },
-            { ".xls", "application_vnd_ms_excel" },
-            { ".docx", "application_msword" },
-            { ".xlsx", "application_vnd_ms_excel" },
-            { ".ppt", "application_vnd_ms_powerpoint" },
-            { ".pps", "application_vnd_ms_powerpoint" },
-            { ".zip", "application_zip" },
-            { ".pdf", "application_pdf" },
-            { ".html", "text_html" },
-            { ".txt", "text_plain" },
-            { ".xml", "text_xml" },
-            { ".jpg", "image_jpeg" },
-            { ".gif", "image_gif" },
-            { ".bmp", "image_bmp" }
-        };
+            { ".doc", "application/msword" },
+            { ".xls", "application/vnd.ms-excel" },
+            { ".docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document" },
+            { ".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" },
+            { ".ppt", "application/vnd.ms-powerpoint" },
+            { ".pps", "application/vnd.ms-powerpoint" },
+            { ".zip", "application/zip" },
+            { ".pdf", "application/pdf" },
+            { ".html", "text/html" },
+            { ".txt", "text/plain" },
+            { ".xml", "text/xml" },
+            { ".jpg", "image/jpeg" },
+            { ".gif", "image/gif" },
+            { ".bmp", "image/bmp" },
+            { ".png", "image/png" }
 
         public static string GetMIMEType(string fileName)
         {
