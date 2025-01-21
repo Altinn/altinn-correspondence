71d83b7ade69577306fdfd3d9ccd6817a84036f6
Ytterås <axel.ytteras@digdir.no>
2025-01-21 15:49:56 +0100

    Made methods in CacheHelpers public such that they can be accesed outside the class

--- src/Altinn.Correspondence.Common/Helpers/CacheHelpers.cs
+++ src/Altinn.Correspondence.Common/Helpers/CacheHelpers.cs
@@ -5,13 +5,13 @@ namespace Altinn.Correspondence.Common.Helpers
 {
     public static class CacheHelpers
     {
-        private static async Task StoreObjectInCacheAsync<T>(string key, T value, IDistributedCache cache, DistributedCacheEntryOptions cacheOptions, CancellationToken cancellationToken = default)
+        public static async Task StoreObjectInCacheAsync<T>(string key, T value, IDistributedCache cache, DistributedCacheEntryOptions cacheOptions, CancellationToken cancellationToken = default)
         {
             string serializedDataString = JsonSerializer.Serialize(value);
             await cache.SetStringAsync(key, serializedDataString, cacheOptions, cancellationToken);
         }
 
-        private static async Task<T?> GetObjectFromCacheAsync<T>(string key, IDistributedCache cache, CancellationToken cancellationToken = default)
+        public static async Task<T?> GetObjectFromCacheAsync<T>(string key, IDistributedCache cache, CancellationToken cancellationToken = default)
         {
             string? cachedDataString = await cache.GetStringAsync(key, cancellationToken);
             if (!string.IsNullOrEmpty(cachedDataString))
