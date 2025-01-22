0c18bbe147435155a16dcab7a86239323b195b3b
Hammerbeck <andreas.hammerbeck@digdir.no>
2025-01-22 14:02:24 +0100

    add validation to links in reply options

--- src/Altinn.Correspondence.API/Models/CorrespondenceReplyOptionExt.cs
+++ src/Altinn.Correspondence.API/Models/CorrespondenceReplyOptionExt.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace Altinn.Correspondence.API.Models
 {
@@ -11,6 +12,7 @@ namespace Altinn.Correspondence.API.Models
         /// <summary>
         /// Gets or sets the URL to be used as a reply/response to a correspondence. 
         /// </summary>
+        [IsLink]
         [JsonPropertyName("linkURL")]
         public required string LinkURL { get; set; }
 
@@ -19,5 +21,42 @@ namespace Altinn.Correspondence.API.Models
         /// </summary>
         [JsonPropertyName("linkText")]
         public string? LinkText { get; set; }
+
+        [AttributeUsage(AttributeTargets.Property)]
+        internal class IsLinkAttribute : ValidationAttribute
+        {
+            public IsLinkAttribute()
+            {
+            }
+
+            protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+            {
+                if (value == null || value.GetType() != typeof(string))
+                {
+                    return new ValidationResult("LinkURL is not of type string");
+                }
+                if (((string)value).Length > 255)
+                {
+                    return new ValidationResult("LinkURL is too long");
+                }
+                if (!Uri.IsWellFormedUriString((string)value, UriKind.Absolute))
+                {
+                    return new ValidationResult("LinkURL is not a valid URL");
+                }
+                if (((string)value).Contains(" "))
+                {
+                    return new ValidationResult("LinkURL contains whitespace");
+                }
+                if (((string)value).StartsWith("http://"))
+                {
+                    return new ValidationResult("LinkURL is not secure");
+                }
+                if (!((string)value).StartsWith("https://"))
+                {
+                    return new ValidationResult("LinkURL must start with https://");
+                }
+                return ValidationResult.Success;
+            }
+        }
     }
 }
