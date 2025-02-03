72bdf4cc4ae6fddccc3a6ca864f9efe7df4706ec
Ytterås <axel.ytteras@digdir.no>
2025-02-03 10:11:14 +0100

    Added list AllowedFileTypes to ApplicationConstants which contains the supported fileTypes for attachments

--- src/Altinn.Correspondence.Application/ApplicationConstants.cs
+++ src/Altinn.Correspondence.Application/ApplicationConstants.cs
@@ -3,4 +3,23 @@ namespace Altinn.Correspondence.Application.Settings;
 public static class ApplicationConstants
 {
     public const long MaxFileUploadSize = 250 * 1024 * 1024;
+    public static readonly List<string> AllowedFileTypes =
+    [
+        ".doc",
+        ".xls",
+        ".docx",
+        ".xlsx",
+        ".ppt",
+        ".pps",
+        ".zip",
+        ".pdf",
+        ".html",
+        ".txt",
+        ".xml",
+        ".jpg",
+        ".gif",
+        ".bmp",
+        ".png",
+        ".json"
+    ];
 }
