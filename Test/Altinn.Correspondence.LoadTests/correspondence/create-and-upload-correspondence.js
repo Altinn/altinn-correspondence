ec9a5de2532c74a6540a8afa91453dcfb6f425f5
Dagfinn Olsen <dagfinno@gmail.com>
2025-02-07 15:34:58 +0100

    chore(performance): implement changes (#690)
    * Change accept headers to application/json and correspondences are listed in ids, not items
    
    * removed console.log

--- Test/Altinn.Correspondence.LoadTests/correspondence/create-and-upload-correspondence.js
+++ Test/Altinn.Correspondence.LoadTests/correspondence/create-and-upload-correspondence.js
@@ -40,7 +40,7 @@ export function uploadCorrespondence(serviceOwner, endUser, traceCalls) {
             Authorization: "Bearer " + getPersonalTokenForServiceOwner(serviceOwner),
             traceparent: traceparent,
             'Content-Type': 'multipart/form-data; boundary=' + formData.boundary,
-            'Accept': '*/*, text/plain',
+            'Accept': '*/*, application/json',
             'Accept-Encoding': 'gzip, deflate, br',
             'Connection': 'keep-alive'
         },
