92e1785ed6b49a865af69b2ace36597505804d47
Ytterås <axel.ytteras@digdir.no>
2025-02-12 13:09:51 +0100

    Updated the notification templates to refelect the change that the $recipientName$ is now the message recipient and not the notification recipient.

--- src/Altinn.Correspondence.Persistence/Migrations/20250129072844_update_notification_templates.cs
+++ src/Altinn.Correspondence.Persistence/Migrations/20250129072844_update_notification_templates.cs
@@ -11,15 +11,33 @@ namespace Altinn.Correspondence.Persistence.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(
-                "UPDATE correspondence.\"NotificationTemplates\" SET \"EmailBody\" = 'Hei $recipientName$, du har mottatt en ny melding fra $sendersName$. {textToken}Logg deg inn i Altinn for å se denne meldingen.',\"ReminderEmailBody\" = 'Hei $recipientName$, dette er en påminnelse om at du har mottatt en ny melding fra $sendersName$. {textToken}Logg deg inn i Altinn for å se denne meldingen.', \"SmsBody\" = 'Hei $recipientName$, du har mottatt en ny melding fra $sendersName$. {textToken}Logg deg inn i Altinn for å se denne meldingen.' ,\"ReminderSmsBody\" = 'Hei $recipientName$, dette er en påminnelse om at du har mottatt en ny melding fra $sendersName$. {textToken}Logg deg inn i Altinn for å se denne meldingen.' Where \"Language\" = 'nb' and \"Template\" = 1");
+                "UPDATE correspondence.\"NotificationTemplates\" SET " +
+                "\"EmailBody\" = 'Hei. $recipientName$ har mottatt en ny melding fra $sendersName$. {textToken}Logg deg inn i Altinn for å se denne meldingen.', " +
+                "\"ReminderEmailBody\" = 'Hei. Dette er en påminnelse om at $recipientName$ har mottatt en ny melding fra $sendersName$. {textToken}Logg deg inn i Altinn for å se denne meldingen.', " +
+                "\"SmsBody\" = 'Hei. $recipientName$ har mottatt en ny melding fra $sendersName$. {textToken}Logg deg inn i Altinn for å se denne meldingen.' ," +
+                "\"ReminderSmsBody\" = 'Hei. Dette er en påminnelse om at $recipientName$ har mottatt en ny melding fra $sendersName$. {textToken}Logg deg inn i Altinn for å se denne meldingen.' Where " +
+                "\"Language\" = 'nb' and " +
+                "\"Template\" = 1");
 
 
             migrationBuilder.Sql(
-                "UPDATE correspondence.\"NotificationTemplates\" SET \"EmailBody\" = 'Hello $recipientName$, you have received a new message from $sendersName$. {textToken}Log in to Altinn to see this message.', \"ReminderEmailBody\" = 'Hello $recipientName$, this is a reminder that you have received a new message from $sendersName$. {textToken}Log in to Altinn to see this message.',\"SmsBody\" = 'Hello $recipientName$, you have received a new message from $sendersName$. {textToken}Log in to Altinn to see this message.', \"ReminderSmsBody\" = 'Hello $recipientName$, this is a reminder that you have received a new message from $sendersName$. {textToken}Log in to Altinn to see this message.' Where \"Language\" = 'en' and \"Template\" = 1");
+                "UPDATE correspondence.\"NotificationTemplates\" SET " +
+                "\"EmailBody\" = 'Hello. $recipientName$ has received a new message from $sendersName$. {textToken}Log in to Altinn to see this message.', " +
+                "\"ReminderEmailBody\" = 'Hello. This is a reminder that $recipientName$ has received a new message from $sendersName$. {textToken}Log in to Altinn to see this message.'," +
+                "\"SmsBody\" = 'Hello. $recipientName$ has received a new message from $sendersName$. {textToken}Log in to Altinn to see this message.', " +
+                "\"ReminderSmsBody\" = 'Hello. This is a reminder that $recipientName$ has received a new message from $sendersName$. {textToken}Log in to Altinn to see this message.' Where " +
+                "\"Language\" = 'en' and " +
+                "\"Template\" = 1");
 
 
             migrationBuilder.Sql(
-                "UPDATE correspondence.\"NotificationTemplates\"  SET \"EmailBody\" = 'Hei $recipientName$, du har motteke ei ny melding frå $sendersName$. {textToken}Logg deg inn i Altinn for å sjå denne meldinga.', \"ReminderEmailBody\" = 'Hei $recipientName$, dette er ei påminning om at du har motteke ei ny melding frå $sendersName$. {textToken}Logg deg inn i Altinn for å sjå denne meldinga.', \"SmsBody\" = 'Hei $recipientName$, du har motteke ei ny melding frå $sendersName$. {textToken}Logg deg inn i Altinn for å sjå denne meldinga.', \"ReminderSmsBody\" = 'Hei $recipientName$, dette er ei påminning om at du har motteke ei ny melding frå $sendersName$. {textToken}Logg deg inn i Altinn for å sjå denne meldinga.' Where \"Language\" = 'nn' and \"Template\" = 1");
+                "UPDATE correspondence.\"NotificationTemplates\"  SET " +
+                "\"EmailBody\" = 'Hei. $recipientName$ har motteke ei ny melding frå $sendersName$. {textToken}Logg deg inn i Altinn for å sjå denne meldinga.', " +
+                "\"ReminderEmailBody\" = 'Hei. Dette er ei påminning om at $recipientName$ har motteke ei ny melding frå $sendersName$. {textToken}Logg deg inn i Altinn for å sjå denne meldinga.', " +
+                "\"SmsBody\" = 'Hei. $recipientName$ har motteke ei ny melding frå $sendersName$. {textToken}Logg deg inn i Altinn for å sjå denne meldinga.', " +
+                "\"ReminderSmsBody\" = 'Hei. Dette er ei påminning om at $recipientName$ har motteke ei ny melding frå $sendersName$. {textToken}Logg deg inn i Altinn for å sjå denne meldinga.' Where " +
+                "\"Language\" = 'nn' and " +
+                "\"Template\" = 1");
         }
 
         /// <inheritdoc />
