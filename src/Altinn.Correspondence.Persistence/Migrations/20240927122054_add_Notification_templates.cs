338dfffce43e99d58a96e3f3feebd8ed211287c1
Ytterås <axel.ytteras@digdir.no>
2025-02-14 10:09:10 +0100

    Revert "Updated the notification templates to refelect the change that the $recipientName$ is now the message recipient and not the notification recipient."
    This reverts commit 92e1785ed6b49a865af69b2ace36597505804d47.

--- src/Altinn.Correspondence.Persistence/Migrations/20240927122054_add_Notification_templates.cs
+++ src/Altinn.Correspondence.Persistence/Migrations/20240927122054_add_Notification_templates.cs
@@ -34,84 +34,40 @@ namespace Altinn.Correspondence.Persistence.Migrations
                 });
 
             migrationBuilder.InsertData(
-            schema: "correspondence",
-            table: "NotificationTemplates",
-            columns: new[] { "EmailBody", "EmailSubject", "Language", "RecipientType", "ReminderEmailBody", "ReminderEmailSubject", "ReminderSmsBody", "SmsBody", "Template" },
-            values: new object[,]
-            {
-                { "{textToken}", "{textToken}", null, null, "{textToken}", "{textToken}", "{textToken}", "{textToken}", 0 },
+           schema: "correspondence",
+           table: "NotificationTemplates",
+           columns: new[] { "EmailBody", "EmailSubject", "Language", "RecipientType", "ReminderEmailBody", "ReminderEmailSubject", "ReminderSmsBody", "SmsBody", "Template" },
+           values: new object[,]
+           {
+                    { "{textToken}", "{textToken}", null, null, "{textToken}", "{textToken}", "{textToken}", "{textToken}", 0 },
+                    { "Hei $recipientName$, du har mottatt en ny melding i Altinn fra $sendersName$. {textToken} Logg deg inn i Altinn inboks for å se denne meldingen.",
+                    "Du har mottatt en melding i Altinn {textToken}", "no", 0, "Hei $recipientName$, dette er en påminnelse om at du har mottatt en ny melding i Altinn fra $sendersName$. {textToken}Logg deg inn i Altinn inboks for å se denne meldingen.",
+                    "Påminnelse - du har mottatt en melding i Altinn {textToken}", "Hei $recipientName$, dette er en påminnelse om at du har mottatt en ny melding i Altinn fra $sendersName$. {textToken} Logg deg inn i Altinn inboks for å se denne meldingen.",
+                     "Hei $recipientName$, du har mottatt en ny melding i Altinn fra $sendersName$. {textToken}Logg deg inn i Altinn inboks for å se denne meldingen.", 1 },
+                    { "Hei $recipientName$, du har mottatt en ny melding i Altinn fra $sendersName$. {textToken}Logg deg inn i Altinn inboks for å se denne meldingen.",
+                    "Du har mottatt en melding i Altinn {textToken}", "no", 1, "Hei $recipientName$, dette er en påminnelse om at du har mottatt en ny melding i Altinn fra $sendersName$. {textToken}Logg deg inn i Altinn inboks for å se denne meldingen.",
+                    "Påminnelse - du har mottatt en melding i Altinn {textToken}", "Hei $recipientName$, dette er en påminnelse om at du har mottatt en ny melding i Altinn fra $sendersName$. {textToken}Logg deg inn i Altinn inboks for å se denne meldingen.",
+                     "Hei $recipientName$, du har mottatt en ny melding i Altinn fra $sendersName$. {textToken}Logg deg inn i Altinn inboks for å se denne meldingen.", 1 },
+                    
+                    //english
+                    { "Hello $recipientName$, you have received a new message in Altinn from $sendersName$. {textToken} Log in to Altinn inbox to see this message.",
+                    "You have received a message in Altinn {textToken}", "en", 0, "Hello $recipientName$, this is a reminder that you have received a new message in Altinn from $sendersName$. {textToken}Log in to Altinn inbox to see this message.",
+                    "Reminder - you have received a message in Altinn {textToken}", "Hello $recipientName$, this is a reminder that you have received a new message in Altinn from $sendersName$. {textToken} Log in to Altinn inbox to see this message.",
+                     "Hello $recipientName$, you have received a new message in Altinn from $sendersName$. {textToken}Log in to Altinn inbox to see this message.", 1 },
+                    { "Hello $recipientName$, you have received a new message in Altinn from $sendersName$. {textToken}Log in to Altinn inbox to see this message.",
+                    "You have received a message in Altinn {textToken}", "en", 1, "Hello $recipientName$, this is a reminder that you have received a new message in Altinn from $sendersName$. {textToken}Log in to Altinn inbox to see this message.",
+                    "Reminder - you have received a message in Altinn {textToken}", "Hello $recipientName$, this is a reminder that you have received a new message in Altinn from $sendersName$. {textToken}Log in to Altinn inbox to see this message.",
+                     "Hello $recipientName$, you have received a new message in Altinn from $sendersName$. {textToken}Log in to Altinn inbox to see this message.", 1 },
 
-                //bokmål
-                { 
-                    "Hei. $recipientName$ har mottatt en ny melding i Altinn fra $sendersName$. {textToken} Logg deg inn i Altinn inboks for å se denne meldingen.",
-                    "$recipientName$ har mottatt en melding i Altinn {textToken}",
-                    "no", 
-                    0, 
-                    "Hei. Dette er en påminnelse om at $recipientName$ har mottatt en ny melding i Altinn fra $sendersName$. {textToken}Logg deg inn i Altinn inboks for å se denne meldingen.",
-                    "Påminnelse - $recipientName$ har mottatt en melding i Altinn {textToken}", 
-                    "Hei. Dette er en påminnelse om at $recipientName$ har mottatt en ny melding i Altinn fra $sendersName$. {textToken} Logg deg inn i Altinn inboks for å se denne meldingen.",
-                    "Hei. $recipientName$ har mottatt en ny melding i Altinn fra $sendersName$. {textToken}Logg deg inn i Altinn inboks for å se denne meldingen.", 
-                    1
-                },
-                { 
-                    "Hei. $recipientName$ har mottatt en ny melding i Altinn fra $sendersName$. {textToken}Logg deg inn i Altinn inboks for å se denne meldingen.",
-                    "$recipientName$ har mottatt en melding i Altinn {textToken}", 
-                    "no", 
-                    1,
-                    "Hei. Dette er en påminnelse om at $recipientName$ har mottatt en ny melding i Altinn fra $sendersName$. {textToken}Logg deg inn i Altinn inboks for å se denne meldingen.",
-                    "Påminnelse - $recipientName$ har mottatt en melding i Altinn {textToken}", 
-                    "Hei. Dette er en påminnelse om at $recipientName$ har mottatt en ny melding i Altinn fra $sendersName$. {textToken}Logg deg inn i Altinn inboks for å se denne meldingen.",
-                    "Hei. $recipientName$ har mottatt en ny melding i Altinn fra $sendersName$. {textToken}Logg deg inn i Altinn inboks for å se denne meldingen.", 
-                    1 
-                },
-
-                //english
-                { 
-                    "Hello. $recipientName$ has received a new message in Altinn from $sendersName$. {textToken} Log in to Altinn inbox to see this message.",
-                    "$recipientName$ has received a message in Altinn {textToken}", 
-                    "en", 
-                    0, 
-                    "Hello. This is a reminder that $recipientName$ has received a new message in Altinn from $sendersName$. {textToken}Log in to Altinn inbox to see this message.",
-                    "Reminder - $recipientName$ has received a message in Altinn {textToken}", 
-                    "Hello. This is a reminder that $recipientName$ has received a new message in Altinn from $sendersName$. {textToken} Log in to Altinn inbox to see this message.",
-                    "Hello. $recipientName$ has received a new message in Altinn from $sendersName$. {textToken}Log in to Altinn inbox to see this message.", 
-                    1 
-                },
-                { 
-                    "Hello. $recipientName$ has received a new message in Altinn from $sendersName$. {textToken}Log in to Altinn inbox to see this message.",
-                    "$recipientName$ has received a message in Altinn {textToken}", 
-                    "en", 
-                    1, 
-                    "Hello. This is a reminder that $recipientName$ has received a new message in Altinn from $sendersName$. {textToken}Log in to Altinn inbox to see this message.",
-                    "Reminder - $recipientName$ has received a message in Altinn {textToken}", 
-                    "Hello. This is a reminder that $recipientName$ has received a new message in Altinn from $sendersName$. {textToken}Log in to Altinn inbox to see this message.",
-                    "Hello $recipientName$ has received a new message in Altinn from $sendersName$. {textToken}Log in to Altinn inbox to see this message.", 
-                    1 
-                },
-
-                //nynorsk
-                { 
-                    "Hei. $recipientName$ har motteke ei ny melding i Altinn frå $sendersName$. {textToken} Logg deg inn i Altinn inboks for å sjå denne meldinga.",
-                    "$recipientName$ har motteke ei melding i Altinn {textToken}", 
-                    "nn", 
-                    0, 
-                    "Hei. Dette er ei påminning om at $recipientName$ har motteke ei ny melding i Altinn frå $sendersName$. {textToken}Logg deg inn i Altinn inboks for å sjå denne meldinga.",
-                    "Påminning - $recipientName$ har motteke ei melding i Altinn {textToken}", 
-                    "Hei. Dette er ei påminning om at $recipientName$ har motteke ei ny melding i Altinn frå $sendersName$. {textToken} Logg deg inn i Altinn inboks for å sjå denne meldinga.",
-                    "Hei. $recipientName$ har motteke ei ny melding i Altinn frå $sendersName$. {textToken}Logg deg inn i Altinn inboks for å sjå denne meldinga.", 
-                    1 
-                },
-                {
-                    "Hei. $recipientName$ har motteke ei ny melding i Altinn frå $sendersName$. {textToken}Logg deg inn i Altinn inboks for å sjå denne meldinga.",
-                    "$recipientName$ har motteke ei melding i Altinn {textToken}", 
-                    "nn", 
-                    1, 
-                    "Hei. Dette er ei påminning om at $recipientName$ har motteke ei ny melding i Altinn frå $sendersName$. {textToken}Logg deg inn i Altinn inboks for å sjå denne meldinga.",
-                    "Påminning - $recipientName$ har motteke ei melding i Altinn {textToken}", 
-                    "Hei. Dette er ei påminning om at $recipientName$ har motteke ei ny melding i Altinn frå $sendersName$. {textToken} Logg deg inn i Altinn inboks for å sjå denne meldinga.",
-                    "Hei. $recipientName$ har motteke ei ny melding i Altinn frå $sendersName$. {textToken}Logg deg inn i Altinn inboks for å sjå denne meldinga.", 
-                    1 
-                }
+                     //nynorsk
+                    { "Hei $recipientName$, du har motteke ei ny melding i Altinn frå $sendersName$. {textToken} Logg deg inn i Altinn inboks for å sjå denne meldinga.",
+                    "Du har motteke ei melding i Altinn {textToken}", "nn", 0, "Hei $recipientName$, dette er ei påminning om at du har motteke ei ny melding i Altinn frå $sendersName$. {textToken}Logg deg inn i Altinn inboks for å sjå denne meldinga.",
+                    "Påminning - du har motteke ei melding i Altinn {textToken}", "Hei $recipientName$, dette er ei påminning om at du har motteke ei ny melding i Altinn frå $sendersName$. {textToken} Logg deg inn i Altinn inboks for å sjå denne meldinga.",
+                     "Hei $recipientName$, du har motteke ei ny melding i Altinn frå $sendersName$. {textToken}Logg deg inn i Altinn inboks for å sjå denne meldinga.", 1 },
+                    { "Hei $recipientName$, du har motteke ei ny melding i Altinn frå $sendersName$. {textToken}Logg deg inn i Altinn inboks for å sjå denne meldinga.",
+                    "Du har motteke ei melding i Altinn {textToken}", "nn", 1, "Hei $recipientName$, dette er ei påminning om at du har motteke ei ny melding i Altinn frå $sendersName$. {textToken}Logg deg inn i Altinn inboks for å sjå denne meldinga.",
+                    "Påminning - du har motteke ei melding i Altinn {textToken}", "Hei $recipientName$, dette er ei påminning om at du har motteke ei ny melding i Altinn frå $sendersName$. {textToken} Logg deg inn i Altinn inboks for å sjå denne meldinga.",
+                     "Hei $recipientName$, du har motteke ei ny melding i Altinn frå $sendersName$. {textToken}Logg deg inn i Altinn inboks for å sjå denne meldinga.", 1 }
            });
         }
 
