9d8bbc060663d3cf989f1c0a2fcb7b2940a048a8
Ytterås <axel.ytteras@digdir.no>
2025-02-24 09:41:01 +0100

    Added database migration which adds IsMigrating flag to the correspondence table

---
+++ src/Altinn.Correspondence.Persistence/Migrations/20250220130047_AddIsMigratingFlagToCorrespondences.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Correspondence.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsMigratingFlagToCorrespondences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMigrating",
+                schema: "correspondence",
+                table: "Correspondences",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMigrating",
+                schema: "correspondence",
+                table: "Correspondences");
+        }
+    }
+}
