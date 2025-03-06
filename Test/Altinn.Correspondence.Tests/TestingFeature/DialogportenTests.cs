ccb363d845192a3d90c4321f44d179c811ee696c
Ytterås <axel.ytteras@digdir.no>
2025-03-06 14:02:48 +0100

    Added unit-test that verifies publishing a correspondence fails if its missing dialogPorten reference

--- Test/Altinn.Correspondence.Tests/TestingFeature/DialogportenTests.cs
+++ Test/Altinn.Correspondence.Tests/TestingFeature/DialogportenTests.cs
@@ -1,4 +1,6 @@
 ﻿using Altinn.Correspondence.API.Models;
+using Altinn.Correspondence.Application;
+using Altinn.Correspondence.Application.Helpers;
 using Altinn.Correspondence.Application.InitializeCorrespondences;
 using Altinn.Correspondence.Application.PublishCorrespondence;
 using Altinn.Correspondence.Common.Constants;
@@ -142,5 +144,55 @@ public class DialogportenTests
 
         // Assert
         Assert.True(hangfireBackgroundJobClient.Invocations.Any(invocation => invocation.Arguments[0].ToString() == "IDialogportenService.PurgeCorrespondenceDialog"));
+    }
+
+    [Fact]
+    public async Task PublishCorrespondence_WithMissingDialogPortenExternalReference_Fails()
+    {
+        // Arrange
+        var hangfireBackgroundJobClient = new Mock<IBackgroundJobClient>();
+        var contactReservationRegistry = new Mock<IContactReservationRegistryService>();
+        contactReservationRegistry.Setup(contactReservationRegistry => contactReservationRegistry.IsPersonReserved(It.IsAny<string>())).ReturnsAsync(false);
+        contactReservationRegistry.Setup(contactReservationRegistry => contactReservationRegistry.GetReservedRecipients(It.IsAny<List<string>>())).ReturnsAsync(new List<string>());
+        var testFactory = new UnitWebApplicationFactory((IServiceCollection services) =>
+        {
+            services.AddSingleton(hangfireBackgroundJobClient.Object);
+            services.AddSingleton(contactReservationRegistry.Object);
+        });
+
+        var correspondence = new CorrespondenceBuilder().CreateCorrespondence().Build();
+        var testClient = testFactory.CreateSenderClient();
+
+        // Act
+        using var scope = testFactory.Services.CreateScope();
+        var correspondenceRepository = scope.ServiceProvider.GetRequiredService<ICorrespondenceRepository>();
+        var initializedCorrespondence = await correspondenceRepository.CreateCorrespondence(new Core.Models.Entities.CorrespondenceEntity()
+        {
+            Created = DateTimeOffset.UtcNow,
+            Recipient = correspondence.Recipients[0],
+            RequestedPublishTime = DateTimeOffset.UtcNow.AddSeconds(-5),
+            ResourceId = correspondence.Correspondence.ResourceId,
+            Sender = correspondence.Correspondence.Sender,
+            SendersReference = correspondence.Correspondence.SendersReference,
+            Statuses = new List<Core.Models.Entities.CorrespondenceStatusEntity>()
+            {
+                new Core.Models.Entities.CorrespondenceStatusEntity()
+                {
+                    Status = Core.Models.Enums.CorrespondenceStatus.ReadyForPublish,
+                    StatusChanged = DateTimeOffset.UtcNow
+                }
+            },
+        }, CancellationToken.None);
+        var correspondenceId = initializedCorrespondence.Id;
+        var handler = scope.ServiceProvider.GetRequiredService<PublishCorrespondenceHandler>();
+        var result = await handler.Process(correspondenceId, null, CancellationToken.None);
+
+        var processedCorrespondence = await correspondenceRepository.GetCorrespondenceById(correspondenceId, false, false, CancellationToken.None);
+
+        // Assert
+        Assert.NotNull(processedCorrespondence);
+        Assert.Contains(processedCorrespondence.Statuses, s => s.Status == Core.Models.Enums.CorrespondenceStatus.Failed);
+        var FailedStatus = processedCorrespondence.Statuses.Find(s => s.Status == Core.Models.Enums.CorrespondenceStatus.Failed);
+        Assert.Equal($"Dialogporten dialog not yet created for correspondence {correspondenceId}", FailedStatus?.StatusText);
     }
 }
