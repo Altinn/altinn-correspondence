30643ab83f5831b0473608d48136f638dc955eb3
Ragnar Inge Fatland <acn-rfatl@ai-dev.no>
2025-03-20 13:57:53 +0100

    Initial draft of external entities for Legacy CorrepondForwarding

---
+++ src/Altinn.Correspondence.API/Models/MigrateCorrespondenceForwardingEventExt.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json.Serialization;
+using Altinn.Correspondence.Common.Constants;
+
+namespace Altinn.Correspondence.API.Models
+{
+    public class MigrateCorrespondenceForwardingEventExt
+    {
+        /// <summary>
+        /// The date and time for when the correspondence was forwarded.
+        /// </summary>
+        [JsonPropertyName("forwardedOnDate")]
+        public required DateTimeOffset ForwardedOnDate { get; set; }
+
+        /// <summary>
+        /// PartyId of user that performed the forwarding action.
+        /// </summary>
+        [JsonPropertyName("forwardedByUserPartyUuid")]
+        public Guid ForwardedByUserPartyUuid { get; set; }
+
+        /// <summary>
+        /// Optional PartyId of user that the correspondence was forwarded to.
+        /// </summary>
+        [JsonPropertyName("forwardedToUserPartyUuid")]
+        public Guid? ForwardedToUserPartyUuid { get; set; }
+
+        /// <summary>
+        /// Optional Text used when forwarding the correspondence.
+        /// </summary>
+        [JsonPropertyName("forwardingText")]
+        public string? ForwardingText { get; set; }
+
+        /// <summary>
+        /// Optional Email address that was used to notify the user that the correspondence was forwarded to.
+        /// </summary>
+        [JsonPropertyName("forwardedToEmailAddress")]
+        public string? ForwardedToEmailAddress { get; set; }
+
+        /// <summary>
+        /// Optional Org number for the Mailbox supplier that the correspondence was forwarded to.
+        /// </summary>
+        [JsonPropertyName("mailboxSupplier")]
+        [OrganizationNumber(ErrorMessage = $"Organization numbers should be on the format '{UrnConstants.OrganizationNumberAttribute}:organizationnumber' or the format countrycode:organizationnumber, for instance 0192:910753614")]        
+        public string? MailboxSupplier { get; set; }
+    }
+}
--- src/Altinn.Correspondence.Application/GetCorrespondenceHistory/LegacyGetCorrespondenceHistoryResponse.cs
+++ src/Altinn.Correspondence.Application/GetCorrespondenceHistory/LegacyGetCorrespondenceHistoryResponse.cs
@@ -1,25 +1,40 @@
-using Altinn.Correspondence.Core.Models.Notifications;
+namespace Altinn.Correspondence.Application.GetCorrespondenceHistory
+{
+    public class LegacyGetCorrespondenceHistoryResponse
+    {
+        public string Status { get; set; }
+        public DateTimeOffset? StatusChanged { get; set; }
+        public string StatusText { get; set; }
+        public LegacyUser User { get; set; }
+        public LegacyNotification? Notification { get; set; }
+        public LegacyForwardingAction? ForwardingAction { get; set; }
+    }
+    public class LegacyUser
+    {
+        public int? PartyId { get; set; }
+        public string? NationalIdentityNumber { get; set; }
+        public string? Name { get; set; }
+    }
+    public class LegacyNotification
+    {
+        public string? EmailAddress { get; set; }
+        public string? MobileNumber { get; set; }
+        public string? OrganizationNumber { get; set; }
+        public string? NationalIdentityNumber { get; set; }
+    }
 
-namespace Altinn.Correspondence.Application.GetCorrespondenceHistory;
+    public class LegacyForwardingAction
+    {
+        public required DateTimeOffset ForwardedOnDate { get; set; }
 
-public class LegacyGetCorrespondenceHistoryResponse
-{
-    public string Status { get; set; }
-    public DateTimeOffset? StatusChanged { get; set; }
-    public string StatusText { get; set; }
-    public LegacyUser User { get; set; }
-    public LegacyNotification? Notification { get; set; }
-}
-public class LegacyUser
-{
-    public int? PartyId { get; set; }
-    public string? NationalIdentityNumber { get; set; }
-    public string? Name { get; set; }
-}
-public class LegacyNotification
-{
-    public string? EmailAddress { get; set; }
-    public string? MobileNumber { get; set; }
-    public string? OrganizationNumber { get; set; }
-    public string? NationalIdentityNumber { get; set; }
+        public int ForwardedByUserPartyId { get; set; }
+
+        public int? ForwardedToUserPartyId { get; set; }
+
+        public string? ForwardingText { get; set; }
+
+        public string? ForwardedToEmailAddress { get; set; }
+
+        public string? MailboxSupplier { get; set; }
+    }
 }
