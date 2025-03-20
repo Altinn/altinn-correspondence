1648eead9498284c77fafd794378afb6d5b24403
Roar Mjelde <roarmj@gmail.com>
2025-03-20 13:08:32 +0100

    Use correct publishtime variable

--- src/Altinn.Correspondence.Application/Helpers/HangfireScheduleHelper.cs
+++ src/Altinn.Correspondence.Application/Helpers/HangfireScheduleHelper.cs
@@ -9,7 +9,7 @@ namespace Altinn.Correspondence.Application.Helpers
         public void SchedulePublish(Guid correspondenceId, DateTimeOffset publishTime, CancellationToken cancellationToken)
         {
             var actualPublishTime = publishTime < DateTimeOffset.UtcNow ? DateTimeOffset.UtcNow : publishTime; // If in past, do now
-            backgroundJobClient.Schedule<PublishCorrespondenceHandler>((handler) => handler.Process(correspondenceId, null, cancellationToken), publishTime);
+            backgroundJobClient.Schedule<PublishCorrespondenceHandler>((handler) => handler.Process(correspondenceId, null, cancellationToken), actualPublishTime);
         }
     }
 }
