a6934afd22438d2064eccc0a0741673e35a6716b
Martin Todorov <>
2025-03-25 09:20:57 +0100

    Primary på confirm

--- src/Altinn.Correspondence.Integrations/Dialogporten/Mappers/CreateDialogRequestMapper.cs
+++ src/Altinn.Correspondence.Integrations/Dialogporten/Mappers/CreateDialogRequestMapper.cs
@@ -249,7 +249,7 @@ namespace Altinn.Correspondence.Integrations.Dialogporten.Mappers
                     Action = "read",
                     Url = $"{baseUrl.TrimEnd('/')}/correspondence/api/v1/correspondence/{correspondence.Id}/confirm",
                     HttpMethod = "POST",
-                    Priority = "Tertiary"
+                    Priority = !correspondence.ReplyOptions?.Any() ?? true ? "Primary" : "Tertiary"
                 });
             }
 
