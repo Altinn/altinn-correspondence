3bd71cb923bb390e8f1f906471249ad814b5dca9
Roar Mjelde <roarmj@gmail.com>
2025-05-02 20:54:37 +0200

    Added StorageProvider to attachment, but as a nullable field for now until we have migrated all existing attachments in future PR.

--- src/Altinn.Correspondence.Core/Models/Entities/ServiceOwnerEntity.cs
+++ src/Altinn.Correspondence.Core/Models/Entities/ServiceOwnerEntity.cs
@@ -5,4 +5,8 @@ public class ServiceOwnerEntity
     public required string Id { get; set; }
     public required string Name { get; set; }
     public required List<StorageProviderEntity> StorageProviders { get; set; }
+    public StorageProviderEntity? GetStorageProvider(bool withVirusScan)
+    {
+        return StorageProviders.FirstOrDefault(sp => sp.Type == (withVirusScan ? Enums.StorageProviderType.Altinn3Azure : Enums.StorageProviderType.Altinn3AzureWithoutVirusScan));
+    }
 }
