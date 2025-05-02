520730ae5898159e98703f448bce2461e12bdd34
Roar Mjelde <roarmj@gmail.com>
2025-05-02 20:54:37 +0200

    Added StorageProvider to attachment, but as a nullable field for now until we have migrated all existing attachments in future PR.

---
+++ src/Altinn.Correspondence.Persistence/Migrations/20250502185314_AddStorageProviderToAttachment.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Correspondence.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStorageProviderToAttachment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "StorageProviderId",
+                schema: "correspondence",
+                table: "Attachments",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attachments_StorageProviderId",
+                schema: "correspondence",
+                table: "Attachments",
+                column: "StorageProviderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attachments_StorageProviders_StorageProviderId",
+                schema: "correspondence",
+                table: "Attachments",
+                column: "StorageProviderId",
+                principalSchema: "correspondence",
+                principalTable: "StorageProviders",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attachments_StorageProviders_StorageProviderId",
+                schema: "correspondence",
+                table: "Attachments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Attachments_StorageProviderId",
+                schema: "correspondence",
+                table: "Attachments");
+
+            migrationBuilder.DropColumn(
+                name: "StorageProviderId",
+                schema: "correspondence",
+                table: "Attachments");
+        }
+    }
+}
