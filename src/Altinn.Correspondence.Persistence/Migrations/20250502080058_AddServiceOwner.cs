3152d596c5978fb735e611031e6460356ced7f97
Roar Mjelde <roarmj@gmail.com>
2025-05-02 15:10:05 +0200

    Fixed it so that we can schedule new service owner with select initialize_service_owner('12345678', 'test')

--- src/Altinn.Correspondence.Persistence/Migrations/20250502080058_AddServiceOwner.cs
+++ src/Altinn.Correspondence.Persistence/Migrations/20250502080058_AddServiceOwner.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 #nullable disable
 
@@ -55,6 +61,71 @@ namespace Altinn.Correspondence.Persistence.Migrations
                 schema: "correspondence",
                 table: "StorageProviders",
                 column: "ServiceOwnerEntityId");
+
+            var sql = @"
+                CREATE OR REPLACE FUNCTION initialize_service_owner(
+                    service_owner_id TEXT,
+                    service_owner_name TEXT
+                )
+                RETURNS INT AS $$
+                DECLARE
+                    jobId INT;
+                    request_json TEXT;
+                    parameter_types_array TEXT[];
+                    parameter_types_string TEXT;
+                BEGIN
+                    -- Create the service owner request JSON
+                    request_json := json_build_object(
+                        'ServiceOwnerId', service_owner_id,
+                        'ServiceOwnerName', service_owner_name
+                    )::text;
+
+                    -- Define the parameter types
+                    parameter_types_array := ARRAY[
+                        'Altinn.Correspondence.Application.InitializeResourceOwner.InitializeServiceOwnerRequest, Altinn.Correspondence.Application, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null',
+                        'System.Security.Claims.ClaimsPrincipal, System.Security.Claims, Version=9.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a',
+                        'System.Threading.CancellationToken, System.Private.CoreLib, Version=9.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e'
+                    ];
+
+                    -- Format parameter types array to JSON string with proper escaping
+                    parameter_types_string := format('[""%s"",""%s"",""%s""]',
+                        replace(parameter_types_array[1], '""', '\""'),
+                        replace(parameter_types_array[2], '""', '\""'),
+                        replace(parameter_types_array[3], '""', '\""')
+                    );
+
+                    -- Insert into hangfire.job
+                    INSERT INTO hangfire.job (invocationdata, arguments, createdat, statename, stateid)
+                    VALUES (
+                        -- Create the invocationdata JSON
+                        json_build_object(
+                            'Type', 'Altinn.Correspondence.Application.InitializeResourceOwner.InitializeServiceOwnerHandler, Altinn.Correspondence.Application, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null',
+                            'Method', 'Process',
+                            'Arguments', format('[""%s"",null,null]',
+                                replace(request_json, '""', '\""') -- Properly escape quotes
+                            ),
+                            'ParameterTypes', parameter_types_string
+                        ),
+
+                        -- Create the arguments as a proper JSON array with the first element as a JSON string
+                        to_jsonb(ARRAY[request_json, NULL, NULL]),
+
+                        now(),
+                        'Enqueued',
+                        NULL
+                    )
+                    RETURNING id INTO jobId;
+
+                    -- Insert into hangfire.jobqueue
+                    INSERT INTO hangfire.jobqueue (jobid, queue, fetchedat)
+                    VALUES (jobId, 'default', NULL);
+
+                    RETURN jobId;
+                END;
+                $$ LANGUAGE plpgsql;
+                ";
+
+            migrationBuilder.Sql(sql);
         }
 
         /// <inheritdoc />
@@ -67,6 +138,8 @@ namespace Altinn.Correspondence.Persistence.Migrations
             migrationBuilder.DropTable(
                 name: "ServiceOwners",
                 schema: "correspondence");
+
+            migrationBuilder.Sql("DROP FUNCTION IF EXISTS initialize_service_owner;");
         }
     }
 }
