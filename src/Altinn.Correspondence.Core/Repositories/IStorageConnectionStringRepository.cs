108cabecc69b6830bf33287376d7f218b6bc6536
Roar Mjelde <roarmj@gmail.com>
2025-05-08 21:15:33 +0200

    More clean-up

--- src/Altinn.Correspondence.Core/Repositories/IStorageConnectionStringRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Altinn.Correspondence.Core.Models.Entities;
-
-namespace Altinn.Correspondence.Core.Repositories
-{
-    public interface IStorageConnectionStringRepository
-    {
-        Task<string> GetStorageConnectionString(StorageProviderEntity storageProviderEntity);
-    }
-}
--- src/Altinn.Correspondence.Integrations/Azure/AzureResourceManagerService.cs
+++ src/Altinn.Correspondence.Integrations/Azure/AzureResourceManagerService.cs
@@ -22,7 +22,7 @@ using System.Text;
 using System.Text.Json;
 
 namespace Altinn.Correspondence.Integrations.Azure;
-public class AzureResourceManagerService : IResourceManager, IStorageConnectionStringRepository
+public class AzureResourceManagerService : IResourceManager
 {
     private readonly AzureResourceManagerOptions _resourceManagerOptions;
     private readonly IHostEnvironment _hostEnvironment;
@@ -31,7 +31,6 @@ public class AzureResourceManagerService : IResourceManager, IStorageConnectionS
     private readonly TokenCredential _credentials;
     private readonly IServiceOwnerRepository _serviceOwnerRepository;
     private readonly IBackgroundJobClient _backgroundJobClient;
-    private readonly SasTokenService _sasTokenService;
     private readonly ILogger<AzureResourceManagerService> _logger;
     private string GetResourceGroupName(string serviceOwnerId) => $"serviceowner-{_resourceManagerOptions.Environment}-{serviceOwnerId.Replace(":", "-")}-rg";
 
@@ -42,7 +41,6 @@ public class AzureResourceManagerService : IResourceManager, IStorageConnectionS
         IServiceOwnerRepository serviceOwnerRepository,
         IHostEnvironment hostingEnvironment,
         IBackgroundJobClient backgroundJobClient,
-        SasTokenService sasTokenCacheService,
         ILogger<AzureResourceManagerService> logger)
     {
         _resourceManagerOptions = resourceManagerOptions.Value;
@@ -51,7 +49,6 @@ public class AzureResourceManagerService : IResourceManager, IStorageConnectionS
         _armClient = new ArmClient(_credentials);
         _serviceOwnerRepository = serviceOwnerRepository;
         _backgroundJobClient = backgroundJobClient;
-        _sasTokenService = sasTokenCacheService;
         _logger = logger;
     }
 
@@ -157,20 +154,6 @@ public class AzureResourceManagerService : IResourceManager, IStorageConnectionS
         return "aicorr" + obfuscationString + "sa";
     }
 
-    public async Task<string> GetStorageConnectionString(StorageProviderEntity storageProviderEntity)
-    {
-        _logger.LogInformation($"Retrieving connection string for storage provider {storageProviderEntity.Id}");
-        if (_hostEnvironment.IsDevelopment())
-        {
-            return "UseDevelopmentStorage=true";
-        }
-        if (storageProviderEntity.StorageResourceName == null)
-        {
-            throw new InvalidOperationException("Storage account has not been deployed");
-        }
-        var sasToken = await _sasTokenService.GetSasToken(storageProviderEntity, storageProviderEntity.StorageResourceName);
-        return $"BlobEndpoint=https://{storageProviderEntity.StorageResourceName}.blob.core.windows.net/attachments?{sasToken}";
-    }
     public async Task UpdateContainerAppIpRestrictionsAsync(Dictionary<string, string> newIps, CancellationToken cancellationToken)
     {
         try 
