49453bbd98681fb8fb4da09c8c2dcd824c2fc05c
taihuy <tientai.cs09@gmail.com>
2025-05-20 00:38:14 +0200

    824 støtte for both som notificationchannel (#998)
    * First version that should work with V2
    
    * Add CreateNotificationHandlerTests and refactor notification handling:
    - Introduced unit tests for CreateNotificationHandler to validate notification creation and reminder logic.
    - Updated CreateNotificationHandler to use NotificationOrderRequestResponseV2 for handling notification responses.
    - Added ShipmentId property to CorrespondenceNotificationEntity for tracking shipment details.
    
    * function works, but not  $correspondenceRecipientName$
    
    * Refactor notification handling to support version 2, including updates to correspondence details and notification status mapping.
    
    * Added creator
    
    * - Improved custom recipient in initialize correspondence
    - Refactored custom recipient test
    
    * added correct status that can decide Succeeded
    
    * Refactored and updated logic for decide succeed in notification status
    
    * Refactored
    
    * added support for both and small bugfiks with DateTimeOffSet to DialogPorten
    
    ---------
    
    Co-authored-by: Tai Huynh <tai.huynh.no@outlook.com>

--- src/Altinn.Correspondence.Core/Models/Enums/NotificationChannel.cs
+++ src/Altinn.Correspondence.Core/Models/Enums/NotificationChannel.cs
@@ -2,9 +2,28 @@ namespace Altinn.Correspondence.Core.Models.Enums;
 
 public enum NotificationChannel
 {
+    /// <summary>   
+    /// The selected channel for the notification is only email.
+    /// </summary>
     Email = 0,
+
+    /// <summary>
+    /// The selected channel for the notification is only sms.
+    /// </summary>
     Sms = 1,
+
+    /// <summary>
+    /// The selected channel for the notification is email preferred.
+    /// </summary>
     EmailPreferred = 2,
+
+    /// <summary>
+    /// The selected channel for the notification is sms preferred.
+    /// </summary>
     SmsPreferred = 3,
+
+    /// <summary>
+    /// The selected channel for the notification is both email and sms.
+    /// </summary>
     EmailAndSms = 4,
 }
