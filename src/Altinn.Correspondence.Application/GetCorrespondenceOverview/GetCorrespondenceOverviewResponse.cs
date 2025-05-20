7e236eedf49be0a6c3736cfef7d75921dbb7bb1f
Ytterås <axel.ytteras@digdir.no>
2025-05-20 13:35:22 +0200

    Added Altinn2CorrespondenceId to CorrespondenceOverviewExt and updated related mappers

--- src/Altinn.Correspondence.Application/GetCorrespondenceOverview/GetCorrespondenceOverviewResponse.cs
+++ src/Altinn.Correspondence.Application/GetCorrespondenceOverview/GetCorrespondenceOverviewResponse.cs
@@ -48,6 +48,8 @@ public class GetCorrespondenceOverviewResponse
     public DateTimeOffset? Published { get; set; }
 
     public bool IsConfirmationNeeded { get; set; }
+
+    public int? Altinn2CorrespondenceId { get; set; }
 }
 
 public class CorrespondenceNotificationOverview {
