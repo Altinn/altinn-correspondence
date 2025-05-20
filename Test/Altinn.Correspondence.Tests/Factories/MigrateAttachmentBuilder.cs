f8512ea9d5fc30e23a7c0ba33f65e7bcf5d02419
tomshag <tom.sverre.hageland@digdir.no>
2025-05-20 13:10:19 +0200

    Satte 2 required properties i MigrateAttachmentBuilder

--- Test/Altinn.Correspondence.Tests/Factories/MigrateAttachmentBuilder.cs
+++ Test/Altinn.Correspondence.Tests/Factories/MigrateAttachmentBuilder.cs
@@ -22,7 +22,9 @@ namespace Altinn.Correspondence.Tests.Factories
                 FileName = "test-file.txt",
                 DisplayName = "Test file",
                 IsEncrypted = false,
-                SenderPartyUuid = new Guid("EBF0DA78-DB7C-4087-9711-2C64DB201EB1")
+                SenderPartyUuid = new Guid("EBF0DA78-DB7C-4087-9711-2C64DB201EB1"),
+                Altinn2AttachmentId = "R1",
+                Created = new DateTimeOffset(new DateTime(2025, 5, 1))
             };
             return this;
         }
