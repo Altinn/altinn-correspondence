2319567e206c39dba9d2832569fc94b787875e76
Ytterås <axel.ytteras@digdir.no>
2025-05-28 09:28:13 +0200

    Added IsConfidential boolean column to correspondences table

---
+++ src/Altinn.Correspondence.Persistence/Migrations/20250527111747_AddIsConfidentialFlag.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Correspondence.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsConfidentialFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsConfidential",
+                schema: "correspondence",
+                table: "Correspondences",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsConfidential",
+                schema: "correspondence",
+                table: "Correspondences");
+        }
+    }
+}
