a356fb09dc80d3f19703a8fc9aa226b01cc9a5e2
Roar Mjelde <36594318+Ceredron@users.noreply.github.com>
2025-06-19 13:40:19 +0200

    Add missing config from performance env (#1082)
    * Add missing index
    
    * Add missing server parameters from yt01

---
+++ src/Altinn.Correspondence.Persistence/Migrations/20250619102557_AddLegacyIndices.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Correspondence.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLegacyIndices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Attachments_DataLocationUrl",
+                schema: "correspondence",
+                table: "Attachments",
+                column: "DataLocationUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Attachments_DataLocationUrl",
+                schema: "correspondence",
+                table: "Attachments");
+        }
+    }
+}
