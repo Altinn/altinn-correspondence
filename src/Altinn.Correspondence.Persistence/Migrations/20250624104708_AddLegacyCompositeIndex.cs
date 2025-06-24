f7436b1dd3e267cb444eb9414cab21566aaa690d
Roar Mjelde <36594318+Ceredron@users.noreply.github.com>
2025-06-24 13:09:37 +0200

    Composite index that includes RequestedPublishTime for legacy search (#1104)
    * Adding a composite index that includes RequestedPublishTime because it is used by legacy search
    
    * Added index with more ef idiomatic syntax
    
    * Added code index too ofc

---
+++ src/Altinn.Correspondence.Persistence/Migrations/20250624104708_AddLegacyCompositeIndex.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Correspondence.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLegacyCompositeIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Correspondences_Recipient_RequestedPublishTime_Id",
+                schema: "correspondence",
+                table: "Correspondences",
+                columns: new[] { "Recipient", "RequestedPublishTime", "Id" },
+                descending: new[] { false, true, false });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Correspondences_Recipient_RequestedPublishTime_Id",
+                schema: "correspondence",
+                table: "Correspondences");
+        }
+    }
+}
