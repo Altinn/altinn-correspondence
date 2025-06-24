c877d6245a0939270128e27b70abe18d0aaf539e
Ytterås <axel.ytteras@digdir.no>
2025-06-24 12:59:14 +0200

    Created and added onBehalfOf validation attribute to the correspondence search endpoint

---
+++ src/Altinn.Correspondence.API/ValidationAttributes/OnBehalfOfAttribute.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using Altinn.Correspondence.Common.Constants;
+using Altinn.Correspondence.Common.Helpers;
+
+namespace Altinn.Correspondence.API.ValidationAttributes;
+public class OnBehalfOfAttribute : ValidationAttribute
+{
+    private static readonly string OrgPattern = $@"^(?:0192:|{UrnConstants.OrganizationNumberAttribute}:)\d{{9}}$";
+    private static readonly string SsnPattern = $@"^(?:{UrnConstants.PersonIdAttribute}:)?\d{{11}}$";
+    private static readonly Regex OrgRegex = new(OrgPattern);
+    private static readonly Regex SsnRegex = new(SsnPattern);
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value == null)
+        {
+            return ValidationResult.Success;
+        }
+
+        if (value is not string stringValue)
+        {
+            return new ValidationResult("OnBehalfOf must be a string");
+        }
+
+        if (string.IsNullOrWhiteSpace(stringValue))
+        {
+            return new ValidationResult("OnBehalfOf cannot be empty");
+        }
+
+        if (OrgRegex.IsMatch(stringValue))
+        {
+            return ValidationResult.Success;
+        }
+
+        if (SsnRegex.IsMatch(stringValue))
+        {
+            if (!stringValue.IsSocialSecurityNumber())
+            {
+                return new ValidationResult("The given OnBehalfOf national identity number is not valid");
+            }
+            return ValidationResult.Success;
+        }
+
+        return new ValidationResult(ErrorMessage ?? 
+            $"OnBehalfOf must be either a valid organization number (format: '{UrnConstants.OrganizationNumberAttribute}:organizationnumber' or '0192:organizationnumber') or a valid national identity number (format: '{UrnConstants.PersonIdAttribute}:nationalidentitynumber' or just 'nationalidentitynumber')");
+    }
+}
