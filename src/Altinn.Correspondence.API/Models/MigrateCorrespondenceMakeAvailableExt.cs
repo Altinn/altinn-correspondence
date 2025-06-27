f302e5e04ce7445d3bc02fb4f1ce181292d76662
tomshag <tom.sverre.hageland@digdir.no>
2025-06-27 09:39:36 +0200

    update for missing jsonpropertynames

--- src/Altinn.Correspondence.API/Models/MigrateCorrespondenceMakeAvailableExt.cs
+++ src/Altinn.Correspondence.API/Models/MigrateCorrespondenceMakeAvailableExt.cs
@@ -1,10 +1,16 @@
+using System.Text.Json.Serialization;
 using Altinn.Correspondence.Core.Models.Entities;
 
 namespace Altinn.Correspondence.API.Models;
 
 public class MigrateCorrespondenceMakeAvailableExt
 {
+    [JsonPropertyName("correspondenceId")]
     public Guid? CorrespondenceId { get; set; }
+    
+    [JsonPropertyName("createEvents")]
     public required bool CreateEvents { get; set; } = false;
+
+    [JsonPropertyName("correspondenceIds")]
     public List<Guid>? CorrespondenceIds { get; set; }
 }
