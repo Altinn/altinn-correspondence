b053e62dd1b4b1fb460f57674b6e0f875e897779
Axel Ytterås <78788499+axely123@users.noreply.github.com>
2025-07-22 09:13:38 +0200

    Feat/controll of all recipients (#1158)
    * Made recipient org roles check apply to all correspondences and changed error message
    
    * Added REPR role as a valid role for correspondences
    
    * Updated related test
    
    * Made roles retrieved for non confidential correspondeces as well
    
    * Changed error message for missing roles
    
    ---------
    
    Co-authored-by: Ytterås <axel.ytteras@digdir.no>

--- src/Altinn.Correspondence.Application/PublishCorrespondence/PublishCorrespondenceHandler.cs
+++ src/Altinn.Correspondence.Application/PublishCorrespondence/PublishCorrespondenceHandler.cs
@@ -137,9 +137,9 @@ public class PublishCorrespondenceHandler(
         {
             errorMessage = $"Recipient of {correspondenceId} is deleted";
         }
-        else if (roles != null && correspondence.IsConfidential && !roles.HasAnyOfRolesOnPerson(ApplicationConstants.RequiredOrganizationRolesForConfidentialCorrespondence))
+        else if (roles != null && !roles.HasAnyOfRolesOnPerson(ApplicationConstants.RequiredOrganizationRolesForCorrespondenceRecipient))
         {
-            errorMessage = $"Recipient of {correspondenceId} is missing required roles to read confidential correspondences";
+            errorMessage = $"Recipient of {correspondenceId} lacks roles required to read correspondences. Consider sending physical mail to this recipient instead.";
         }
         CorrespondenceStatusEntity status;
         AltinnEventType eventType = AltinnEventType.CorrespondencePublished;
@@ -250,10 +250,7 @@ public class PublishCorrespondenceHandler(
             try
             {
                 details = await brregService.GetOrganizationDetails(correspondence.Recipient.WithoutPrefix(), cancellationToken);
-                if (correspondence.IsConfidential)
-                {
-                    roles = await brregService.GetOrganizationRoles(correspondence.Recipient.WithoutPrefix(), cancellationToken);
-                }
+                roles = await brregService.GetOrganizationRoles(correspondence.Recipient.WithoutPrefix(), cancellationToken);
             }
             catch (BrregNotFoundException)
             {
@@ -261,7 +258,7 @@ public class PublishCorrespondenceHandler(
                 {
                     var subOrganizationDetails = await brregService.GetSubOrganizationDetails(correspondence.Recipient.WithoutPrefix(), cancellationToken);
                     details = subOrganizationDetails;
-                    if (correspondence.IsConfidential && subOrganizationDetails.ParentOrganizationNumber != null)
+                    if (subOrganizationDetails.ParentOrganizationNumber != null)
                     {
                         roles = await brregService.GetOrganizationRoles(subOrganizationDetails.ParentOrganizationNumber, cancellationToken);
                     }
