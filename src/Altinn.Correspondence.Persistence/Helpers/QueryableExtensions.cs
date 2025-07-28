e293a62b555db9efda0c42b08fb81f4e0fc8751c
Roar Mjelde <36594318+Ceredron@users.noreply.github.com>
2025-07-28 09:35:30 +0200

    Added AttachmentsDownloaded as an active status in legacy search (#1166)

--- src/Altinn.Correspondence.Persistence/Helpers/QueryableExtensions.cs
+++ src/Altinn.Correspondence.Persistence/Helpers/QueryableExtensions.cs
@@ -76,6 +76,7 @@ namespace Altinn.Correspondence.Persistence.Helpers
                     statusesToFilter.Add(CorrespondenceStatus.Read);
                     statusesToFilter.Add(CorrespondenceStatus.Confirmed);
                     statusesToFilter.Add(CorrespondenceStatus.Replied);
+                    statusesToFilter.Add(CorrespondenceStatus.AttachmentsDownloaded);
                 }
 
                 if (includeArchived) // Include correspondences with active status
