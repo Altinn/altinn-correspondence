577935c14765bc6f30e39988f5c6f39d3389a822
Martin Todorov <>
2025-07-30 13:00:10 +0200

    updated migration

--- src/Altinn.Correspondence.Persistence/Migrations/20250730074954_ActivateCron.cs
+++ src/Altinn.Correspondence.Persistence/Migrations/20250730074954_ActivateCron.cs
@@ -13,21 +13,29 @@ namespace Altinn.Correspondence.Persistence.Migrations
             migrationBuilder.Sql(@"
                 DO $do$
                 BEGIN
-                    BEGIN
-                        -- Try to create the extension
-                        CREATE EXTENSION IF NOT EXISTS pg_cron;
-                        
-                        -- If successful, schedule the weekly ANALYZE job
-                        PERFORM cron.schedule(
-                          'weekly_analyze',
-                          '0 4 * * 0',
-                          $$ ANALYZE; $$
-                        );
-                    EXCEPTION
-                        WHEN OTHERS THEN
-                            -- Log the error but don't fail the migration
-                            RAISE NOTICE 'pg_cron could not be activated: %', SQLERRM;
-                    END;
+                    -- Try to create the extension
+                    EXECUTE 'CREATE EXTENSION IF NOT EXISTS pg_cron';
+                EXCEPTION
+                    WHEN OTHERS THEN
+                        -- Log the error but don't fail the migration
+                        RAISE NOTICE 'pg_cron extension creation failed: %', SQLERRM;
+                END
+                $do$;
+            ");
+            
+            migrationBuilder.Sql(@"
+                DO $do$
+                BEGIN
+                    -- Schedule the weekly ANALYZE job
+                    PERFORM cron.schedule(
+                      'weekly_analyze',
+                      '0 4 * * 0',
+                      $$ ANALYZE; $$
+                    );
+                EXCEPTION
+                    WHEN OTHERS THEN
+                        -- Log the error but don't fail the migration
+                        RAISE NOTICE 'pg_cron scheduling failed: %', SQLERRM;
                 END
                 $do$;
             ");
@@ -39,17 +47,25 @@ namespace Altinn.Correspondence.Persistence.Migrations
             migrationBuilder.Sql(@"
                 DO $do$
                 BEGIN
-                    BEGIN
-                        -- Try to unschedule the job first
-                        PERFORM cron.unschedule('weekly_analyze');
-                        
-                        -- Then remove the extension
-                        DROP EXTENSION IF EXISTS pg_cron;
-                    EXCEPTION
-                        WHEN OTHERS THEN
-                            -- Log the error but don't fail the migration
-                            RAISE NOTICE 'pg_cron cleanup failed: %', SQLERRM;
-                    END;
+                    -- Try to unschedule the job first
+                    PERFORM cron.unschedule('weekly_analyze');
+                EXCEPTION
+                    WHEN OTHERS THEN
+                        -- Log the error but don't fail the migration
+                        RAISE NOTICE 'pg_cron unschedule failed: %', SQLERRM;
+                END
+                $do$;
+            ");
+            
+            migrationBuilder.Sql(@"
+                DO $do$
+                BEGIN
+                    -- Try to remove the extension
+                    EXECUTE 'DROP EXTENSION IF EXISTS pg_cron';
+                EXCEPTION
+                    WHEN OTHERS THEN
+                        -- Log the error but don't fail the migration
+                        RAISE NOTICE 'pg_cron extension removal failed: %', SQLERRM;
                 END
                 $do$;
             ");
