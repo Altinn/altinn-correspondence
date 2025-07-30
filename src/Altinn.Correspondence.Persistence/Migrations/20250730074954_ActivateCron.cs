79c4261dbab3e894392fc0489f8ae2aace8791ac
Martin Todorov <>
2025-07-30 09:59:34 +0200

    Added database migration

---
+++ src/Altinn.Correspondence.Persistence/Migrations/20250730074954_ActivateCron.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Correspondence.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActivateCron : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE EXTENSION IF NOT EXISTS pg_cron;");
+            
+            migrationBuilder.Sql(@"
+                SELECT cron.schedule(
+                  'weekly_analyze',
+                  '0 4 * * 0',
+                  $$ ANALYZE; $$
+                );
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("SELECT cron.unschedule('weekly_analyze');");
+        }
+    }
+}
