0a6eb706bcb32ab2eb1d1c3e442da0c464092749
Martin Todorov <>
2025-07-31 12:33:51 +0200

    Revert "simplified code"
    This reverts commit 14e52a10619c4fe5f9692591ed150bbe0b12d696.

--- src/Altinn.Correspondence.Persistence/Migrations/20250730074954_ActivateCron.cs
+++ src/Altinn.Correspondence.Persistence/Migrations/20250730074954_ActivateCron.cs
@@ -13,13 +13,19 @@ namespace Altinn.Correspondence.Persistence.Migrations
             migrationBuilder.Sql(@"
                 DO $do$
                 BEGIN
-                    CREATE EXTENSION pg_cron;
+                    -- Try to create the extension
+                    CREATE EXTENSION IF NOT EXISTS pg_cron;
                     
+                    -- If successful, schedule the weekly ANALYZE job
                     PERFORM cron.schedule(
                       'weekly_analyze',
                       '0 4 * * 0',
                       $$ ANALYZE; $$
                     );
+                EXCEPTION
+                    WHEN OTHERS THEN
+                        -- Log the error but don't fail the migration
+                        RAISE NOTICE 'pg_cron could not be activated: %', SQLERRM;
                 END
                 $do$;
             ");
@@ -31,9 +37,15 @@ namespace Altinn.Correspondence.Persistence.Migrations
             migrationBuilder.Sql(@"
                 DO $do$
                 BEGIN
+                    -- Try to unschedule the job first
                     PERFORM cron.unschedule('weekly_analyze');
                     
-                    DROP EXTENSION pg_cron;
+                    -- Then try to remove the extension
+                    DROP EXTENSION IF EXISTS pg_cron;
+                EXCEPTION
+                    WHEN OTHERS THEN
+                        -- Log the error but don't fail the migration
+                        RAISE NOTICE 'pg_cron cleanup failed: %', SQLERRM;
                 END
                 $do$;
             ");
