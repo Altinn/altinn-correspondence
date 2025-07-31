11a54c1a43e649110b1b6ddf13d68906ae5be3ca
Martin Todorov <>
2025-07-31 07:34:48 +0200

    minimal test

--- src/Altinn.Correspondence.Persistence/Migrations/20250730074954_ActivateCron.cs
+++ src/Altinn.Correspondence.Persistence/Migrations/20250730074954_ActivateCron.cs
@@ -10,65 +10,13 @@ namespace Altinn.Correspondence.Persistence.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(@"
-                DO $do$
-                BEGIN
-                    -- Try to create the extension
-                    EXECUTE 'CREATE EXTENSION IF NOT EXISTS pg_cron';
-                EXCEPTION
-                    WHEN OTHERS THEN
-                        -- Log the error but don't fail the migration
-                        RAISE NOTICE 'pg_cron extension creation failed: %', SQLERRM;
-                END
-                $do$;
-            ");
-            
-            migrationBuilder.Sql(@"
-                DO $do$
-                BEGIN
-                    -- Schedule the weekly ANALYZE job
-                    PERFORM cron.schedule(
-                      'weekly_analyze',
-                      '0 4 * * 0',
-                      $$ ANALYZE; $$
-                    );
-                EXCEPTION
-                    WHEN OTHERS THEN
-                        -- Log the error but don't fail the migration
-                        RAISE NOTICE 'pg_cron scheduling failed: %', SQLERRM;
-                END
-                $do$;
-            ");
+            migrationBuilder.Sql("CREATE EXTENSION IF NOT EXISTS pg_cron;");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(@"
-                DO $do$
-                BEGIN
-                    -- Try to unschedule the job first
-                    PERFORM cron.unschedule('weekly_analyze');
-                EXCEPTION
-                    WHEN OTHERS THEN
-                        -- Log the error but don't fail the migration
-                        RAISE NOTICE 'pg_cron unschedule failed: %', SQLERRM;
-                END
-                $do$;
-            ");
-            
-            migrationBuilder.Sql(@"
-                DO $do$
-                BEGIN
-                    -- Try to remove the extension
-                    EXECUTE 'DROP EXTENSION IF EXISTS pg_cron';
-                EXCEPTION
-                    WHEN OTHERS THEN
-                        -- Log the error but don't fail the migration
-                        RAISE NOTICE 'pg_cron extension removal failed: %', SQLERRM;
-                END
-                $do$;
-            ");
+            migrationBuilder.Sql("DROP EXTENSION IF EXISTS pg_cron;");
         }
     }
 }
