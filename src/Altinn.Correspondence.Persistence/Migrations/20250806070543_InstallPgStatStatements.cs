4f9a0d2572669bf49cec0f4a0b9f1ce3b1704d6c
Martin Todorov <>
2025-08-06 09:13:24 +0200

    removed exception handling

--- src/Altinn.Correspondence.Persistence/Migrations/20250806070543_InstallPgStatStatements.cs
+++ src/Altinn.Correspondence.Persistence/Migrations/20250806070543_InstallPgStatStatements.cs
@@ -13,21 +13,7 @@ namespace Altinn.Correspondence.Persistence.Migrations
             migrationBuilder.Sql(@"
                 DO $do$
                 BEGIN
-                    -- Try to create the pg_stat_statements extension
                     CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
-                    
-                    -- Verify the extension is installed
-                    IF NOT EXISTS (
-                        SELECT 1 FROM pg_extension WHERE extname = 'pg_stat_statements'
-                    ) THEN
-                        RAISE EXCEPTION 'pg_stat_statements extension could not be installed';
-                    END IF;
-                    
-                    RAISE NOTICE 'pg_stat_statements extension installed successfully';
-                EXCEPTION
-                    WHEN OTHERS THEN
-                        -- Log the error but don't fail the migration
-                        RAISE NOTICE 'pg_stat_statements could not be installed: %', SQLERRM;
                 END
                 $do$;
             ");
