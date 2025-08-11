804fe1941e18704f0657440d261375c85c834251
Martin Todorov <>
2025-08-11 10:42:40 +0200

    update migration 2

--- src/Altinn.Correspondence.Persistence/Migrations/20250806070543_InstallPgStatStatements.cs
+++ src/Altinn.Correspondence.Persistence/Migrations/20250806070543_InstallPgStatStatements.cs
@@ -10,14 +10,35 @@ namespace Altinn.Correspondence.Persistence.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // Note: pg_stat_statements extension requires manual installation
-            // Run: CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+            migrationBuilder.Sql(@"
+                DO $do$
+                BEGIN
+                    -- Try to create the extension
+                    CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+                EXCEPTION
+                    WHEN OTHERS THEN
+                        -- Log the error but don't fail the migration
+                        RAISE NOTICE 'pg_stat_statements could not be activated: %', SQLERRM;
+                END
+                $do$;
+            ");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            // Run: DROP EXTENSION IF EXISTS pg_stat_statements;
+            migrationBuilder.Sql(@"
+                DO $do$
+                BEGIN
+                    -- Try to remove the extension
+                    DROP EXTENSION IF EXISTS pg_stat_statements;
+                EXCEPTION
+                    WHEN OTHERS THEN
+                        -- Log the error but don't fail the migration
+                        RAISE NOTICE 'pg_stat_statements cleanup failed: %', SQLERRM;
+                END
+                $do$;
+            ");
         }
     }
 }
