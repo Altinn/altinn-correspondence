dcc9fb219f5f5bfab6d40dceef1e07a526e29279
Roar Mjelde <roarmj@gmail.com>
2025-09-07 12:34:54 +0200

    Actually added migration

---
+++ src/Altinn.Correspondence.Persistence/Migrations/20250907103403_CorrespondenceIdStatusIndexOnCorrespondenceStatus.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Correspondence.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrespondenceIdStatusIndexOnCorrespondenceStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CorrespondenceStatuses_CorrespondenceId",
+                schema: "correspondence",
+                table: "CorrespondenceStatuses");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CorrespondenceStatuses_CorrespondenceId_Status",
+                schema: "correspondence",
+                table: "CorrespondenceStatuses",
+                columns: new[] { "CorrespondenceId", "Status" },
+                descending: new[] { false, true });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CorrespondenceStatuses_CorrespondenceId_Status",
+                schema: "correspondence",
+                table: "CorrespondenceStatuses");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CorrespondenceStatuses_CorrespondenceId",
+                schema: "correspondence",
+                table: "CorrespondenceStatuses",
+                column: "CorrespondenceId");
+        }
+    }
+}
