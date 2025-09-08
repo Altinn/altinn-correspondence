42e3635bc9937d5af2acb67f845a8d6f4c796de8
tomshag <tom.sverre.hageland@digdir.no>
2025-09-08 11:43:31 +0200

    added a comment to synceventtype enum to make clear it's only used to sync to altinn 2

--- src/Altinn.Correspondence.Core/Models/Enums/SyncEventType.cs
+++ src/Altinn.Correspondence.Core/Models/Enums/SyncEventType.cs
@@ -1,4 +1,8 @@
 namespace Altinn.Correspondence.Core.Models.Enums;
+
+/// <summary>
+/// This Enum is used when syncing specific events from Altinn 3 to Altinn 2.
+/// </summary>
 public enum SyncEventType
 {
     Read,
