d12e8274bf243057a50ad24c91c78777e5d5eafd
Roar Mjelde <roarmj@gmail.com>
2025-09-21 23:12:29 +0200

    Perishing should have the longer timeout

--- src/Altinn.Correspondence.Application/CleanupPerishingDialogs/CleanupPerishingDialogsHandler.cs
+++ src/Altinn.Correspondence.Application/CleanupPerishingDialogs/CleanupPerishingDialogsHandler.cs
@@ -31,7 +31,7 @@ public class CleanupPerishingDialogsHandler(
     }
 
     [AutomaticRetry(Attempts = 0)]
-    [DisableConcurrentExecution(timeoutInSeconds: 1800)]
+    [DisableConcurrentExecution(timeoutInSeconds: 14400)]
     public async Task ExecuteCleanupInBackground(int windowSize, CancellationToken cancellationToken)
     {
         logger.LogInformation("Executing cleanup of perishing dialogs in background job");
