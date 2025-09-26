85f6541d7c3c8e35c83105b967ec4855bf904f40
Ytterås <axel.ytteras@digdir.no>
2025-09-26 12:58:52 +0200

    Added altinn register service method to retrieve correspondence party roles

--- src/Altinn.Correspondence.Integrations/Altinn/Register/AltinnRegisterService.cs
+++ src/Altinn.Correspondence.Integrations/Altinn/Register/AltinnRegisterService.cs
@@ -5,6 +5,7 @@ using Altinn.Correspondence.Common.Helpers;
 using Altinn.Correspondence.Core.Options;
 using Altinn.Correspondence.Core.Services;
 using Altinn.Platform.Register.Models;
+using Altinn.Correspondence.Core.Models.Register;
 using Microsoft.Extensions.Caching.Hybrid;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -179,6 +180,23 @@ public class AltinnRegisterService : IAltinnRegisterService
         }
 
         return party;
+    }
+
+    public async Task<List<RoleItem>> LookUpPartyRoles(int partyId, CancellationToken cancellationToken = default)
+    {
+        var response = await _httpClient.GetAsync($"register/api/v1/parties/{partyId}/roles/correspondence-roles", cancellationToken);
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Error when looking up party roles in Altinn Register.Statuscode was: {response.StatusCode}, error was: {await response.Content.ReadAsStringAsync()}");
+        }
+
+        var roles = await response.Content.ReadFromJsonAsync<Roles>(cancellationToken: cancellationToken);
+        if (roles is null)
+        {
+            throw new Exception("Unexpected json response when looking up party roles in Altinn Register");
+        }
+
+        return roles.Data ?? new List<RoleItem>();
     }
 
     public async Task<List<Party>?> LookUpPartiesByIds(List<string> identificationIds, CancellationToken cancellationToken = default)
