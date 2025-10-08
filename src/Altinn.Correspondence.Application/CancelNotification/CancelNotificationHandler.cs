2390ef8905e57f3dae4dbbdcef95e15435f191ea
Ytterås <axel.ytteras@digdir.no>
2025-10-08 12:24:27 +0200

    corrected log and removed redundant attribute

--- src/Altinn.Correspondence.Application/CancelNotification/CancelNotificationHandler.cs
+++ src/Altinn.Correspondence.Application/CancelNotification/CancelNotificationHandler.cs
@@ -1,15 +1,9 @@
-using Altinn.Correspondence.Core.Models.Entities;
 using Altinn.Correspondence.Core.Repositories;
 using Altinn.Correspondence.Core.Services;
 using Altinn.Correspondence.Core.Services.Enums;
 using Hangfire;
-using Hangfire.Server;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Slack.Webhooks;
 using System.Runtime.CompilerServices;
-using System.Security.Claims;
-using Altinn.Correspondence.Core.Options;
 
 [assembly: InternalsVisibleTo("Altinn.Correspondence.Tests")]
 namespace Altinn.Correspondence.Application.CancelNotification
@@ -17,24 +11,14 @@ namespace Altinn.Correspondence.Application.CancelNotification
     public class CancelNotificationHandler(
         ILogger<CancelNotificationHandler> logger,
         ICorrespondenceRepository correspondenceRepository,
-        IBackgroundJobClient backgroundJobClient,
-        IHostEnvironment hostEnvironment)
+        IBackgroundJobClient backgroundJobClient)
     {
-        private const string RetryCountKey = "RetryCount";
-        private const int MaxRetries = 10;
-
-        [AutomaticRetry(Attempts = MaxRetries)]
-        public async Task Process(PerformContext context, Guid correspondenceId, ClaimsPrincipal? _, CancellationToken cancellationToken = default)
+        public async Task Process(Guid correspondenceId, CancellationToken cancellationToken = default)
         {
             var operationTimestamp = DateTimeOffset.UtcNow;
-
-            logger.LogInformation("Cancelling notifications for correspondence {correspondenceId}", correspondenceId);
+            logger.LogInformation("Sending information activity to Dialogporten that the notifications for correspondence {correspondenceId} have been cancelled", correspondenceId);
             var correspondence = await correspondenceRepository.GetCorrespondenceById(correspondenceId, false, false, false, cancellationToken);
             var notificationEntities = correspondence?.Notifications ?? [];
-            await CancelNotification(correspondenceId, notificationEntities, operationTimestamp, cancellationToken);
-        }
-        public async Task CancelNotification(Guid correspondenceId, List<CorrespondenceNotificationEntity> notificationEntities, DateTimeOffset operationTimestamp, CancellationToken cancellationToken)
-        {
             foreach (var notification in notificationEntities)
             {
                 if (notification.RequestedSendTime <= DateTimeOffset.UtcNow) continue; // Notification has already been sent
