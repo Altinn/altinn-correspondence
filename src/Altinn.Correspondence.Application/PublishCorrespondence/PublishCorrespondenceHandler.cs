d2ec682e13788c79ca73dcda7746b8e72f487c6d
Ytterås <axel.ytteras@digdir.no>
2025-10-08 08:21:58 +0200

    test publish failure

--- src/Altinn.Correspondence.Application/PublishCorrespondence/PublishCorrespondenceHandler.cs
+++ src/Altinn.Correspondence.Application/PublishCorrespondence/PublishCorrespondenceHandler.cs
@@ -85,7 +85,7 @@ public class PublishCorrespondenceHandler(
         bool hasDialogportenDialog = correspondence!.ExternalReferences.Any(reference => reference.ReferenceType == ReferenceType.DialogportenDialogId);
         logger.LogInformation("Correspondence {CorrespondenceId} has Dialogporten dialog: {HasDialog}", correspondenceId, hasDialogportenDialog);
 
-        var errorMessage = "";
+        var errorMessage = "testing";
         if (correspondence == null)
         {
             errorMessage = "Correspondence " + correspondenceId + " not found when publishing";
