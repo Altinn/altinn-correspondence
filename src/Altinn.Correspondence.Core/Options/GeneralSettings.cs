f3c72150e8c2601a283cbca9b0ce99ccfebefe2f
Roar Mjelde <roarmj@gmail.com>
2025-10-14 12:08:41 +0200

    Remove the resource whitelist as performance validated

--- src/Altinn.Correspondence.Core/Options/GeneralSettings.cs
+++ src/Altinn.Correspondence.Core/Options/GeneralSettings.cs
@@ -9,7 +9,6 @@ public class GeneralSettings
     public string AltinnSblBridgeBaseUrl { get; set; } = string.Empty;
     public string RedisConnectionString { get; set; } = string.Empty;
     public string ContactReservationRegistryBaseUrl { get; set; } = string.Empty;
-    public string ResourceWhitelist { get; set; } = string.Empty;
     public string BrregBaseUrl { get; set; } = string.Empty;
     public string ApplicationInsightsConnectionString { get; set; } = string.Empty;
     public bool DisableTelemetryForMigration { get; set; } = true;
