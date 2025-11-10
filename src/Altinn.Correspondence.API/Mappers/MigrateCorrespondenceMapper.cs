ac8e0e1d972a0d4741c0ef64023acb7804b7a8f2
Magnus Åsheim <magnus.hansen.asheim@digdir.no>
2025-11-10 13:54:48 +0100

    revert old changes

--- src/Altinn.Correspondence.API/Mappers/MigrateCorrespondenceMapper.cs
+++ src/Altinn.Correspondence.API/Mappers/MigrateCorrespondenceMapper.cs
@@ -40,7 +40,7 @@ internal static class MigrateCorrespondenceMapper
                 MessageBody = migrateCorrespondenceExt.CorrespondenceData.Correspondence.Content.MessageBody,
                 Attachments = []
             } : null,
-            IsConfirmationNeeded = IsCorrespondenceConfirmed(migrateCorrespondenceExt.EventHistory) ? false : true,
+            IsConfirmationNeeded = migrateCorrespondenceExt.CorrespondenceData.Correspondence.IsConfirmationNeeded,
             IsMigrating = migrateCorrespondenceExt.IsMigrating,
             PartyId = migrateCorrespondenceExt.PartyId,
             Published = migrateCorrespondenceExt.CorrespondenceData.Correspondence.Published
@@ -239,10 +239,5 @@ internal static class MigrateCorrespondenceMapper
             Altinn2NotificationId = notificationExt.Altinn2NotificationId,
             IsReminder = notificationExt.IsReminder,
         };
-    }
-
-    private static bool IsCorrespondenceConfirmed(List<MigrateCorrespondenceStatusEventExt> statusHistory)
-    {
-        return statusHistory.Any(statusEvent => statusEvent.Status == MigrateCorrespondenceStatusExt.Confirmed);
     }
 }
