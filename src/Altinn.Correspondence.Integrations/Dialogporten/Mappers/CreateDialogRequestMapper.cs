0f4204c36d5c2993af6f5b9a55366843a0dee16e
Roar Mjelde <roar.mjelde@digdir.no>
2025-11-11 11:10:20 +0100

    Use input datetime for now to ensure future test compatibility

--- src/Altinn.Correspondence.Integrations/Dialogporten/Mappers/CreateDialogRequestMapper.cs
+++ src/Altinn.Correspondence.Integrations/Dialogporten/Mappers/CreateDialogRequestMapper.cs
@@ -38,7 +38,7 @@ namespace Altinn.Correspondence.Integrations.Dialogporten.Mappers
                 ServiceResource = UrnConstants.Resource + ":" + correspondence.ResourceId,
                 Party = correspondence.GetRecipientUrn(),
                 CreatedAt = correspondence.Created,
-                UpdatedAt = correspondence.Published != null && correspondence.Published < DateTime.UtcNow ? correspondence.Published : null,
+                UpdatedAt = correspondence.Published != null && correspondence.Published < currentDateTimeUtcNow ? correspondence.Published : null,
                 VisibleFrom = correspondence.RequestedPublishTime < currentDateTimeUtcNow.AddMinutes(1) ? null : correspondence.RequestedPublishTime,
                 Process = correspondence.ExternalReferences.FirstOrDefault(reference => reference.ReferenceType == ReferenceType.DialogportenProcessId)?.ReferenceValue,
                 DueAt = dueAt,
