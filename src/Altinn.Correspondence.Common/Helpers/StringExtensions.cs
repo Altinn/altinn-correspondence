531dd115cc504062fe2baf95c9ed084eeadef5cf
Roar Mjelde <roar.mjelde@digdir.no>
2025-12-12 09:38:22 +0100

    Null safety

--- src/Altinn.Correspondence.Common/Helpers/StringExtensions.cs
+++ src/Altinn.Correspondence.Common/Helpers/StringExtensions.cs
@@ -53,7 +53,11 @@ public static class StringExtensions
     /// <returns>True if string starts with the party URN prefix, false otherwise.</returns>
     public static bool IsPartyId(this string identifier)
     {
-        return (!string.IsNullOrWhiteSpace(identifier) && identifier.StartsWith(UrnConstants.Party)) || (identifier.Length == 8 && identifier.All(character => character.IsDigit()));
+        if (string.IsNullOrWhiteSpace(identifier))
+        {
+            return false;
+        }
+        return identifier.StartsWith(UrnConstants.Party) || (identifier?.Length == 8 && identifier.All(character => character.IsDigit());
     }
     /// <summary>
     /// Extracts the identifier from a colon-separated string that may contain a prefix.
