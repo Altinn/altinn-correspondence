7f1a286dbddc7eeb942abbd75194b014101e100b
Axel Ytterås <axel.ytteraas@gmail.com>
2025-12-19 10:37:44 +0100

    corrected nullability

--- src/Altinn.Correspondence.Integrations/Altinn/AccessManagement/AltinnAccessManagementService.cs
+++ src/Altinn.Correspondence.Integrations/Altinn/AccessManagement/AltinnAccessManagementService.cs
@@ -82,8 +82,8 @@ public class AltinnAccessManagementService : IAltinnAccessManagementService
                 {
                     PartyId = p.partyId,
                     PartyUuid = p.partyUuid,
-                    OrgNumber = p.organizationNumber!,
-                    SSN = p.personId!,
+                    OrgNumber = p.organizationNumber,
+                    SSN = p.personId,
                     Resources = p.authorizedResources ?? [],
                     PartyTypeName = GetType(p.type!),
                 });
