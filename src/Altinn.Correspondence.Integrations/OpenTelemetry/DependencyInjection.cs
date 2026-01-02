861db5f0bfa9a8f91453663b225535721914756e
Roar Mjelde <roar.mjelde@digdir.no>
2026-01-02 15:15:10 +0100

    Attempt defining processor on exporter

--- src/Altinn.Correspondence.Integrations/OpenTelemetry/DependencyInjection.cs
+++ src/Altinn.Correspondence.Integrations/OpenTelemetry/DependencyInjection.cs
@@ -65,7 +65,7 @@ public static class DependencyInjection
                 metrics.AddAzureMonitorMetricExporter(o => o.ConnectionString = generalSettings.ApplicationInsightsConnectionString));
 
             services.ConfigureOpenTelemetryTracerProvider(tracing =>
-                tracing.AddAzureMonitorTraceExporter(o => o.ConnectionString = generalSettings.ApplicationInsightsConnectionString));
+                tracing.AddAzureMonitorTraceExporter(o => o.ConnectionString = generalSettings.ApplicationInsightsConnectionString).AddProcessor(new HttpDependencyNameProcessor()));
 
             services.ConfigureOpenTelemetryLoggerProvider(logging =>
                 logging.AddAzureMonitorLogExporter(o => o.ConnectionString = generalSettings.ApplicationInsightsConnectionString));
