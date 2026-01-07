3bb93bc3d3337b2d4aa189b626c752ef52714e51
Roar Mjelde <roar.mjelde@digdir.no>
2026-01-07 11:40:18 +0100

    Cache legacy blob service client

--- src/Altinn.Correspondence.Persistence/Repositories/StorageRepository.cs
+++ src/Altinn.Correspondence.Persistence/Repositories/StorageRepository.cs
@@ -21,6 +21,7 @@ namespace Altinn.Correspondence.Persistence.Repositories
         private readonly ILogger<StorageRepository> _logger;
         private readonly ConcurrentDictionary<string, BlobServiceClient> _blobServiceClients;
         private readonly BlobClientOptions _blobClientOptions;
+        private BlobServiceClient? _legacyBlobServiceClient;
 
         private const int BLOCK_SIZE = 32 * 1024 * 1024; // 32 MB
         private const int CONCURRENT_UPLOAD_THREADS = 3;
@@ -70,15 +71,11 @@ namespace Altinn.Correspondence.Persistence.Repositories
             {
                 _logger.LogInformation("Using Correspondence's storage account");
                 var connectionString = _options.ConnectionString;
-                var blobServiceClient = new BlobServiceClient(connectionString,
-                    new BlobClientOptions()
-                    {
-                        Retry =
-                            {
-                                NetworkTimeout = TimeSpan.FromHours(1),
-                            }
-                    });
-                return blobServiceClient.GetBlobContainerClient("attachments");
+                if (_legacyBlobServiceClient is null)
+                {
+                    _legacyBlobServiceClient = new BlobServiceClient(connectionString, _blobClientOptions);
+                }
+                return _legacyBlobServiceClient.GetBlobContainerClient("attachments");
             }
         }
 
