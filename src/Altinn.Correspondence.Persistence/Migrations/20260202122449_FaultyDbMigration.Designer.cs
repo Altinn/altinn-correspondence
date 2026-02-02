7dd56630a47d5b46ef15a9aed18b014e53cd7003
Magnus Åsheim <magnus.hansen.asheim@digdir.no>
2026-02-02 13:27:38 +0100

    add a faulty db migration

--- src/Altinn.Correspondence.Persistence/Migrations/20260202122449_FaultyDbMigration.Designer.cs
+++ src/Altinn.Correspondence.Persistence/Migrations/20260202122449_FaultyDbMigration.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Altinn.Correspondence.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Altinn.Correspondence.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260202122449_FaultyDbMigration")]
+    partial class FaultyDbMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -454,7 +457,7 @@ namespace Altinn.Correspondence.Persistence.Migrations
                     b.Property<Guid>("PartyUuid")
                         .HasColumnType("uuid");
 
-                    b.Property<int>("StatusRenamed")
+                    b.Property<int>("Status")
                         .HasColumnType("integer");
 
                     b.Property<DateTimeOffset>("StatusChanged")
