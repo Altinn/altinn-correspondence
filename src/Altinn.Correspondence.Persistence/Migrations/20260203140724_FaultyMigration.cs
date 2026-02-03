18eb322e64be750385dd0f5c90004eb00e602aa0
Magnus Åsheim <magnus.hansen.asheim@digdir.no>
2026-02-03 15:09:12 +0100

    add final faulty db migration to test workflow

---
+++ src/Altinn.Correspondence.Persistence/Migrations/20260203140724_FaultyMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Correspondence.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FaultyMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Published",
+                table: "Correspondences",
+                schema: "correspondence");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Published",
+                table: "Correspondences",
+                schema: "correspondence",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+    }
+}
