a130da6ec648277569af167a777801ee509286ec
Magnus Åsheim <magnus.hansen.asheim@digdir.no>
2026-02-04 10:06:07 +0100

    test with valid migration

---
+++ src/Altinn.Correspondence.Persistence/Migrations/20260204090546_TestValidMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Correspondence.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestValidMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TestProperty",
+                schema: "correspondence",
+                table: "Correspondences",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TestProperty",
+                schema: "correspondence",
+                table: "Correspondences");
+        }
+    }
+}
