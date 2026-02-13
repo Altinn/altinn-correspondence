8875d1f1e73b7d5bafbf653523183b95ec03e3f3
Axel Ytterås <axel.ytteraas@gmail.com>
2026-02-13 11:01:20 +0100

    log when partyid not found

--- src/Altinn.Correspondence.Integrations/Altinn/Register/AltinnRegisterService.cs
+++ src/Altinn.Correspondence.Integrations/Altinn/Register/AltinnRegisterService.cs
@@ -261,6 +261,7 @@ public class AltinnRegisterService : IAltinnRegisterService
             if (response.StatusCode == System.Net.HttpStatusCode.NotFound || 
                 response.StatusCode == System.Net.HttpStatusCode.NoContent)
             {
+                _logger.LogWarning("IdentificationIds {identificationIds} is not a valid identificator", identificationIds);
                 return new List<Party>();
             }
             
