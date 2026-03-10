5f1c9677f648f7f21af227c18613e1028ec1a71c
Ceredron <roar.mjelde@digdir.no>
2026-03-10 16:37:11 +0100

    Revert "add values for the notifications response when initializing a correspondence (#1779)"
    This reverts commit 52aaf0405546db9588c8050386aabec4f602b397.

--- src/Altinn.Correspondence.API/Models/InitializedCorrespondencesNotificationsExt.cs
+++ src/Altinn.Correspondence.API/Models/InitializedCorrespondencesNotificationsExt.cs
@@ -1,6 +1,5 @@
 
 using System.Text.Json.Serialization;
-using Altinn.Correspondence.Core.Models.Enums;
 
 namespace Altinn.Correspondence.API.Models;
 /// <summary>
@@ -12,14 +11,12 @@ public class InitializedCorrespondencesNotificationsExt
     /// The order ID of the notification
     /// </summary>
     [JsonPropertyName("orderId")]
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public Guid? OrderId { get; set; }
 
     /// <summary>
     /// Boolean indicating if the notification is a reminder
     /// </summary>
     [JsonPropertyName("isReminder")]
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? IsReminder { get; set; }
 
     /// <summary>
@@ -27,29 +24,11 @@ public class InitializedCorrespondencesNotificationsExt
     /// </summary>
     [JsonPropertyName("status")]
     public InitializedNotificationStatusExt Status { get; set; }
-
-    /// <summary>
-    /// The notification channels of the notification 
-    /// </summary>
-    [JsonPropertyName("notificationChannels")]
-    public List<string>? NotificationChannels { get; set; }
-
-    /// <summary>
-    /// The notification template of the notification
-    /// </summary>
-    [JsonPropertyName("notificationTemplate")]
-    public NotificationTemplate? NotificationTemplate { get; set; }
-
-    /// <summary>
-    /// Boolean indicating if a reminder should be sent for the notification
-    /// </summary>    
-    [JsonPropertyName("sendReminder")]
-    public bool SendReminder { get; set; }
 }
 public enum InitializedNotificationStatusExt
 {
     /// <summary>
-    /// The recipient lookup was successful for at least one recipient and the notification order was successful
+    /// The recipient lookup was successful for at least one recipient
     /// </summary>
     Success,
     /// <summary>
