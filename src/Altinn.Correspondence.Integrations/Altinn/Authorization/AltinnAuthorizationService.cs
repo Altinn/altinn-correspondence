15ca9d1cf2b5852bac4befd3de95f7448519ae65
Ceredron <roar.mjelde@digdir.no>
2026-03-16 15:09:06 +0100

    Reapply "Fix build"
    This reverts commit c39fb6ebe33dcbea4bf72cf72a1666116e0a9453.

--- src/Altinn.Correspondence.Integrations/Altinn/Authorization/AltinnAuthorizationService.cs
+++ src/Altinn.Correspondence.Integrations/Altinn/Authorization/AltinnAuthorizationService.cs
@@ -276,7 +276,6 @@ public class AltinnAuthorizationService : IAltinnAuthorizationService
 
     private async Task<XacmlJsonResponse> AuthorizeRequest(XacmlJsonRequestRoot jsonRequest, CancellationToken cancellationToken)
     {
-        _logger.LogDebug("AuthorizeRequest called with request containing {attributeCount} categories", jsonRequest.Request?.Attributes?.Count ?? 0);
         var response = await _httpClient.PostAsJsonAsync("authorization/api/v1/authorize", jsonRequest, cancellationToken);
         if (!response.IsSuccessStatusCode)
         {
@@ -385,7 +384,6 @@ public class AltinnAuthorizationService : IAltinnAuthorizationService
 
     private static string GetActionId(ResourceAccessLevel right)
     {
-        _logger.LogDebug("GetActionId called for right {right}", right);
         return right switch
         {
             ResourceAccessLevel.Read => "read",
