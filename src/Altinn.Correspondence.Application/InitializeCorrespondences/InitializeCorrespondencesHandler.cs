b11f99fcb5965101b0a1b55aacbe29c2c6a63676
Ceredron <roar.mjelde@digdir.no>
2026-04-21 12:39:20 +0200

    Fix build

--- src/Altinn.Correspondence.Application/InitializeCorrespondences/InitializeCorrespondencesHandler.cs
+++ src/Altinn.Correspondence.Application/InitializeCorrespondences/InitializeCorrespondencesHandler.cs
@@ -5,16 +5,12 @@ using Altinn.Correspondence.Application.Helpers;
 using Altinn.Correspondence.Application.UnreadConfidentialCorrespondence;
 using Altinn.Correspondence.Common.Caching;
 using Altinn.Correspondence.Common.Helpers;
-using Altinn.Correspondence.Core.Exceptions;
 using Altinn.Correspondence.Core.Models.Entities;
 using Altinn.Correspondence.Core.Models.Enums;
 using Altinn.Correspondence.Core.Repositories;
 using Altinn.Correspondence.Core.Services;
 using Altinn.Correspondence.Core.Services.Enums;
-<<<<<<< hotfix/log-full-request
-=======
 using Altinn.Correspondence.Persistence.Helpers;
->>>>>>> main
 using Hangfire;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Hybrid;
@@ -22,7 +18,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using OneOf;
 using System.Security.Claims;
-using Altinn.Correspondence.Application.ExpireAttachment;
 
 namespace Altinn.Correspondence.Application.InitializeCorrespondences;
 
