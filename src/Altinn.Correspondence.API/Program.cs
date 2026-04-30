563b58bb751c96045b5509dfb9515597cc6f55d6
Axel Ytterås <78788499+axely123@users.noreply.github.com>
2026-04-30 13:13:05 +0200

    Show propertyDetails additional properties in swagger  (#1892)
    * Specify additional properties with filter and add validation error response as a possible response
    
    * CodeRabbit suggestion

--- src/Altinn.Correspondence.API/Program.cs
+++ src/Altinn.Correspondence.API/Program.cs
@@ -155,6 +155,8 @@ static void ConfigureServices(IServiceCollection services, IConfiguration config
         var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
         var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
         options.IncludeXmlComments(xmlPath);
+        options.SchemaFilter<ProblemDetailsSchemaFilter>();
+        options.OperationFilter<BadRequestOneOfOperationFilter>();
     });
 
     services.AddApplicationHandlers();
