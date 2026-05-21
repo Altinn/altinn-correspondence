bfcb063050bd3a25e85bfc3c2cfe61c92f5a79f9
Roar Mjelde <36594318+Ceredron@users.noreply.github.com>
2026-05-21 11:03:17 +0200

    Only check role when IsConfidential is set (#1958)
    Co-authored-by: Ceredron <roar.mjelde@digdir.no>

--- src/Altinn.Correspondence.Application/Helpers/AltinnRegisterServiceExtensions.cs
+++ src/Altinn.Correspondence.Application/Helpers/AltinnRegisterServiceExtensions.cs
@@ -6,26 +6,22 @@ namespace Altinn.Correspondence.Application.Helpers
 {
     public static class AltinnRegisterServiceExtensions
     {
-        public static async Task<bool> HasRequiredOrganizationRoles(this IAltinnRegisterService altinnRegisterService, string partyUuid, bool isConfidential, CancellationToken cancellationToken)
+        public static async Task<bool> HasRequiredOrganizationRoles(this IAltinnRegisterService altinnRegisterService, string partyUuid, CancellationToken cancellationToken)
         {
             var roles = await altinnRegisterService.LookUpPartyRoles(partyUuid, cancellationToken);
-            if (isConfidential)
-            {
-                return roles.Any(r => ApplicationConstants.RequiredOrganizationRolesForConfidentialCorrespondenceRecipient.Contains(r.Role.Identifier));
-            }
-            return roles.Any(r => ApplicationConstants.RequiredOrganizationRolesForCorrespondenceRecipient.Contains(r.Role.Identifier));
+            return roles.Any(r => ApplicationConstants.RequiredOrganizationRolesForConfidentialCorrespondenceRecipient.Contains(r.Role.Identifier));
         }
 
-        public static async Task<bool> HasPartyRequiredRoles(this IAltinnRegisterService altinnRegisterService, string recipientUrn, Guid partyUuid, bool isConfidential, CancellationToken cancellationToken)
+        public static async Task<bool> HasPartyRequiredRolesForConfidential(this IAltinnRegisterService altinnRegisterService, string recipientUrn, Guid partyUuid, CancellationToken cancellationToken)
         {
             var mainUnits = await altinnRegisterService.LookUpMainUnits(recipientUrn.WithoutPrefix().WithUrnPrefix(), cancellationToken);
             if (!mainUnits.Any())
             {
-                return await altinnRegisterService.HasRequiredOrganizationRoles(partyUuid.ToString(), isConfidential, cancellationToken);
+                return await altinnRegisterService.HasRequiredOrganizationRoles(partyUuid.ToString(), cancellationToken);
             }
             else
             {
-                return await altinnRegisterService.HasRequiredOrganizationRoles(mainUnits.First().PartyUuid.ToString(), isConfidential, cancellationToken);
+                return await altinnRegisterService.HasRequiredOrganizationRoles(mainUnits.First().PartyUuid.ToString(), cancellationToken);
             }
         }
     }
