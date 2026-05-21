a61dbc8b168e5a4895101c98e593d57a776859ee
Ceredron <roar.mjelde@digdir.no>
2026-05-21 12:02:33 +0200

    Revert "APIM openAPI"
    This reverts commit 2e6dac1d41c8e7898eba41bb3daa8d55f867785c.

--- src/Altinn.Correspondence.API/Swagger/OpenApiDocumentationHelper.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc.ApiExplorer;
-
-namespace Altinn.Correspondence.API.Swagger;
-
-internal static class OpenApiDocumentationHelper
-{
-    internal static bool IsExcludedFromPublicOpenApi(ApiDescription description) =>
-        description.ActionDescriptor.EndpointMetadata.OfType<ExcludeFromPublicOpenApiAttribute>().Any();
-}
