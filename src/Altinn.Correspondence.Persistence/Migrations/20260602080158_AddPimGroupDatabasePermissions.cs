4a0564a4eaef65cde68af13e6f6a5dca0d0bddb2
Ceredron <roar.mjelde@digdir.no>
2026-06-03 13:15:03 +0200

    Updated migration to also set audit logs, and only run when the roles have already been set in the database

--- src/Altinn.Correspondence.Persistence/Migrations/20260602080158_AddPimGroupDatabasePermissions.cs
+++ src/Altinn.Correspondence.Persistence/Migrations/20260602080158_AddPimGroupDatabasePermissions.cs
@@ -8,85 +8,135 @@ namespace Altinn.Correspondence.Persistence.Migrations
     public partial class AddPimGroupDatabasePermissions : Migration
     {
         /// <inheritdoc />
+
+        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(
-            @"
+            migrationBuilder.Sql(@"
                 DO $do$
                 DECLARE
-                    db_reader_role_name CONSTANT text := 'correspondence-prod-db-reader';
-                    db_writer_role_name CONSTANT text := 'correspondence-prod-db-writer';
-                    role_name text;
+                    db_reader_role_name CONSTANT text := 'correspondence-prod-db-read';
+                    db_writer_role_name CONSTANT text := 'correspondence-prod-db-write';
+                    reader_exists boolean;
+                    writer_exists boolean;
                 BEGIN
-                    FOREACH role_name IN ARRAY ARRAY[db_reader_role_name, db_writer_role_name]
-                    LOOP
-                        IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = role_name) THEN
-                            BEGIN
-                                PERFORM * FROM pgaadauth_create_principal(role_name, false, false);
-                                RAISE NOTICE 'Created AAD principal for %', role_name;
-                            EXCEPTION
-                                WHEN undefined_function THEN
-                                    EXECUTE format('CREATE ROLE %I', role_name);
-                                    RAISE NOTICE 'pgaadauth_create_principal missing, created local role %', role_name;
-                                WHEN duplicate_object THEN
-                                    RAISE NOTICE 'Role % already exists, skipping', role_name;
-                                WHEN OTHERS THEN
-                                    RAISE NOTICE 'Could not create role %: %', role_name, SQLERRM;
-                            END;
-                        END IF;
-                    END LOOP;
+                    SELECT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = db_reader_role_name) INTO reader_exists;
+                    SELECT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = db_writer_role_name) INTO writer_exists;
+
+                    ----------------------------------------------------------------
+                    -- READER role
+                    ----------------------------------------------------------------
+                    IF reader_exists THEN
+                        GRANT CONNECT, TEMPORARY ON DATABASE correspondence TO ""correspondence-prod-db-read"";
+
+                        GRANT USAGE ON SCHEMA correspondence TO ""correspondence-prod-db-read"";
+                        GRANT SELECT ON ALL TABLES IN SCHEMA correspondence TO ""correspondence-prod-db-read"";
+                        GRANT SELECT ON ALL SEQUENCES IN SCHEMA correspondence TO ""correspondence-prod-db-read"";
+                        GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA correspondence TO ""correspondence-prod-db-read"";
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence GRANT SELECT ON TABLES TO ""correspondence-prod-db-read"";
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence GRANT SELECT ON SEQUENCES TO ""correspondence-prod-db-read"";
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence GRANT EXECUTE ON FUNCTIONS TO ""correspondence-prod-db-read"";
+
+                        ALTER ROLE ""correspondence-prod-db-read"" SET pgaudit.log = 'all';
+
+                        RAISE NOTICE 'Granted privileges and configured pgaudit for correspondence-prod-db-read';
+                    ELSE
+                        RAISE NOTICE 'Role correspondence-prod-db-read does not exist, skipping';
+                    END IF;
+
+                    ----------------------------------------------------------------
+                    -- WRITER role
+                    ----------------------------------------------------------------
+                    IF writer_exists THEN
+                        GRANT CONNECT, TEMPORARY ON DATABASE correspondence TO ""correspondence-prod-db-write"";
+
+                        GRANT USAGE ON SCHEMA correspondence TO ""correspondence-prod-db-write"";
+                        GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER
+                            ON ALL TABLES IN SCHEMA correspondence TO ""correspondence-prod-db-write"";
+                        GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA correspondence TO ""correspondence-prod-db-write"";
+                        GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA correspondence TO ""correspondence-prod-db-write"";
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence
+                            GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER ON TABLES TO ""correspondence-prod-db-write"";
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence
+                            GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO ""correspondence-prod-db-write"";
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence GRANT EXECUTE ON FUNCTIONS TO ""correspondence-prod-db-write"";
+                        GRANT CREATE ON SCHEMA correspondence TO ""correspondence-prod-db-write"";
+
+                        ALTER ROLE ""correspondence-prod-db-write"" SET pgaudit.log = 'all';
+
+                        RAISE NOTICE 'Granted privileges and configured pgaudit for correspondence-prod-db-write';
+                    ELSE
+                        RAISE NOTICE 'Role correspondence-prod-db-write does not exist, skipping';
+                    END IF;
                 END
                 $do$;
-
-                GRANT CONNECT, TEMPORARY ON DATABASE correspondence TO ""correspondence-prod-db-reader"", ""correspondence-prod-db-writer"";
-
-                GRANT USAGE ON SCHEMA correspondence TO ""correspondence-prod-db-reader"", ""correspondence-prod-db-writer"";
-                GRANT SELECT ON ALL TABLES IN SCHEMA correspondence TO ""correspondence-prod-db-reader"";
-                GRANT SELECT ON ALL SEQUENCES IN SCHEMA correspondence TO ""correspondence-prod-db-reader"";
-                GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA correspondence TO ""correspondence-prod-db-reader"";
-                ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence GRANT SELECT ON TABLES TO ""correspondence-prod-db-reader"";
-                ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence GRANT SELECT ON SEQUENCES TO ""correspondence-prod-db-reader"";
-                ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence GRANT EXECUTE ON FUNCTIONS TO ""correspondence-prod-db-reader"";
-
-                GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER
-                    ON ALL TABLES IN SCHEMA correspondence TO ""correspondence-prod-db-writer"";
-                GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA correspondence TO ""correspondence-prod-db-writer"";
-                GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA correspondence TO ""correspondence-prod-db-writer"";
-                ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence
-                    GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER ON TABLES TO ""correspondence-prod-db-writer"";
-                ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence
-                    GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO ""correspondence-prod-db-writer"";
-                ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence GRANT EXECUTE ON FUNCTIONS TO ""correspondence-prod-db-writer"";
-            "
-            );
+            ");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(
-            @"
-                REVOKE CONNECT, TEMPORARY ON DATABASE correspondence FROM ""correspondence-prod-db-reader"", ""correspondence-prod-db-writer"";
+            migrationBuilder.Sql(@"
+                DO $do$
+                DECLARE
+                    db_reader_role_name CONSTANT text := 'correspondence-prod-db-read';
+                    db_writer_role_name CONSTANT text := 'correspondence-prod-db-write';
+                    reader_exists boolean;
+                    writer_exists boolean;
+                BEGIN
+                    SELECT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = db_reader_role_name) INTO reader_exists;
+                    SELECT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = db_writer_role_name) INTO writer_exists;
 
-                REVOKE USAGE ON SCHEMA correspondence FROM ""correspondence-prod-db-reader"", ""correspondence-prod-db-writer"";
-                REVOKE SELECT ON ALL TABLES IN SCHEMA correspondence FROM ""correspondence-prod-db-reader"";
-                REVOKE SELECT ON ALL SEQUENCES IN SCHEMA correspondence FROM ""correspondence-prod-db-reader"";
-                REVOKE EXECUTE ON ALL FUNCTIONS IN SCHEMA correspondence FROM ""correspondence-prod-db-reader"";
-                ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence REVOKE SELECT ON TABLES FROM ""correspondence-prod-db-reader"";
-                ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence REVOKE SELECT ON SEQUENCES FROM ""correspondence-prod-db-reader"";
-                ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence REVOKE EXECUTE ON FUNCTIONS FROM ""correspondence-prod-db-reader"";
+                    ----------------------------------------------------------------
+                    -- READER role
+                    ----------------------------------------------------------------
+                    IF reader_exists THEN
+                        ALTER ROLE ""correspondence-prod-db-read"" RESET pgaudit.log;
 
-                REVOKE SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER
-                    ON ALL TABLES IN SCHEMA correspondence FROM ""correspondence-prod-db-writer"";
-                REVOKE USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA correspondence FROM ""correspondence-prod-db-writer"";
-                REVOKE EXECUTE ON ALL FUNCTIONS IN SCHEMA correspondence FROM ""correspondence-prod-db-writer"";
-                ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence
-                    REVOKE SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER ON TABLES FROM ""correspondence-prod-db-writer"";
-                ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence
-                    REVOKE USAGE, SELECT, UPDATE ON SEQUENCES FROM ""correspondence-prod-db-writer"";
-                ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence REVOKE EXECUTE ON FUNCTIONS FROM ""correspondence-prod-db-writer"";
-            "
-            );
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence REVOKE SELECT ON TABLES FROM ""correspondence-prod-db-read"";
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence REVOKE SELECT ON SEQUENCES FROM ""correspondence-prod-db-read"";
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence REVOKE EXECUTE ON FUNCTIONS FROM ""correspondence-prod-db-read"";
+
+                        REVOKE SELECT ON ALL TABLES IN SCHEMA correspondence FROM ""correspondence-prod-db-read"";
+                        REVOKE SELECT ON ALL SEQUENCES IN SCHEMA correspondence FROM ""correspondence-prod-db-read"";
+                        REVOKE EXECUTE ON ALL FUNCTIONS IN SCHEMA correspondence FROM ""correspondence-prod-db-read"";
+                        REVOKE USAGE ON SCHEMA correspondence FROM ""correspondence-prod-db-read"";
+
+                        REVOKE CONNECT, TEMPORARY ON DATABASE correspondence FROM ""correspondence-prod-db-read"";
+
+                        RAISE NOTICE 'Revoked privileges and reset pgaudit for correspondence-prod-db-read';
+                    ELSE
+                        RAISE NOTICE 'Role correspondence-prod-db-read does not exist, skipping';
+                    END IF;
+
+                    ----------------------------------------------------------------
+                    -- WRITER role
+                    ----------------------------------------------------------------
+                    IF writer_exists THEN
+                        ALTER ROLE ""correspondence-prod-db-write"" RESET pgaudit.log;
+
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence
+                            REVOKE SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER ON TABLES FROM ""correspondence-prod-db-write"";
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence
+                            REVOKE USAGE, SELECT, UPDATE ON SEQUENCES FROM ""correspondence-prod-db-write"";
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA correspondence REVOKE EXECUTE ON FUNCTIONS FROM ""correspondence-prod-db-write"";
+
+                        REVOKE CREATE ON SCHEMA correspondence FROM ""correspondence-prod-db-write"";
+                        REVOKE SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER
+                            ON ALL TABLES IN SCHEMA correspondence FROM ""correspondence-prod-db-write"";
+                        REVOKE USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA correspondence FROM ""correspondence-prod-db-write"";
+                        REVOKE EXECUTE ON ALL FUNCTIONS IN SCHEMA correspondence FROM ""correspondence-prod-db-write"";
+                        REVOKE USAGE ON SCHEMA correspondence FROM ""correspondence-prod-db-write"";
+
+                        REVOKE CONNECT, TEMPORARY ON DATABASE correspondence FROM ""correspondence-prod-db-write"";
+
+                        RAISE NOTICE 'Revoked privileges and reset pgaudit for correspondence-prod-db-write';
+                    ELSE
+                        RAISE NOTICE 'Role correspondence-prod-db-write does not exist, skipping';
+                    END IF;
+                END
+                $do$;
+            ");
         }
     }
 }
