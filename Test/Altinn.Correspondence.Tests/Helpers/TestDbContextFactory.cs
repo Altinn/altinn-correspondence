7866d8568a48a37a932896ea020e2d1a6e3da7e4
Ceredron <roar.mjelde@digdir.no>
2026-06-19 15:15:07 +0200

    Remove PostgresTestcontainerFixture and rely on same postgres server for all tests (from compose)

--- Test/Altinn.Correspondence.Tests/Helpers/TestDbContextFactory.cs
+++ Test/Altinn.Correspondence.Tests/Helpers/TestDbContextFactory.cs
@@ -1,18 +1,55 @@
 using Altinn.Correspondence.Persistence;
 using Altinn.Correspondence.Persistence.Helpers;
+using Altinn.Correspondence.Tests.Fixtures;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
 
 namespace Altinn.Correspondence.Tests.Helpers;
 
 public static class TestDbContextFactory
 {
-    public static ApplicationDbContext Create()
+    // Matches docker-compose.yml and appsettings.Development.json
+    private const string ConnectionString =
+        "Host=localhost:5432;Username=postgres;Password=postgres;Database=correspondence";
+
+    private static readonly Lazy<DbContextOptions<ApplicationDbContext>> Options = new(BuildOptions);
+
+    public static TestApplicationDbContext Create() => new(Options.Value);
+
+    public static TestApplicationDbContext Create(int maxRetryCount)
     {
         var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-            .UseNpgsql("Host=localhost;Database=test;Username=test;Password=test", npgsql =>
-                npgsql.ExecutionStrategy(dependencies =>
-                    new CorrespondenceNpgsqlRetryingExecutionStrategy(dependencies)))
+            .UseNpgsql(ConnectionString, npgsql =>
+            {
+                npgsql.MigrationsAssembly("Altinn.Correspondence.Persistence");
+                ConfigureExecutionStrategy(npgsql, maxRetryCount);
+            })
             .Options;
-        return new ApplicationDbContext(options);
+        return new TestApplicationDbContext(options);
+    }
+
+    private static DbContextOptions<ApplicationDbContext> BuildOptions()
+    {
+        return new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseNpgsql(ConnectionString, npgsql =>
+            {
+                npgsql.MigrationsAssembly("Altinn.Correspondence.Persistence");
+                ConfigureExecutionStrategy(npgsql, maxRetryCount: 0);
+            })
+            .Options;
+    }
+
+    private static void ConfigureExecutionStrategy(NpgsqlDbContextOptionsBuilder npgsql, int maxRetryCount)
+    {
+        if (maxRetryCount == 0)
+        {
+            npgsql.ExecutionStrategy(dependencies => new NonRetryingExecutionStrategy(dependencies));
+        }
+        else
+        {
+            npgsql.ExecutionStrategy(dependencies =>
+                new CorrespondenceNpgsqlRetryingExecutionStrategy(dependencies));
+        }
     }
 }
