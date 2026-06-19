1f2f242507a500c821a62099880a2a69ff537147
Ceredron <roar.mjelde@digdir.no>
2026-06-19 14:17:53 +0200

    Minor fixes

--- src/Altinn.Correspondence.Persistence/Helpers/CorrespondenceNpgsqlRetryingExecutionStrategy.cs
+++ src/Altinn.Correspondence.Persistence/Helpers/CorrespondenceNpgsqlRetryingExecutionStrategy.cs
@@ -2,7 +2,6 @@ using Hangfire;
 using Hangfire.PostgreSql;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
-using Npgsql;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
 using System.Transactions;
 
@@ -22,7 +21,6 @@ public class CorrespondenceNpgsqlRetryingExecutionStrategy : NpgsqlRetryingExecu
     {
         return base.ShouldRetryOn(exception)
             || exception is TransactionAbortedException
-            || exception is PostgresException
             || exception is DbUpdateConcurrencyException
             || exception is BackgroundJobClientException
             || exception is PostgreSqlDistributedLockException;
