40b45cb861b4767adb17d7751814bc88e0cd5b7d
Magnus Åsheim <magnus.hansen.asheim@digdir.no>
2026-07-23 09:25:46 +0200

    add temporary table for IsMigrating correspondences

---
+++ src/Altinn.Correspondence.Persistence/Migrations/20260722132513_Add_Temp_Table_For_IsMigrating.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Correspondence.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Temp_Table_For_IsMigrating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE TABLE ""correspondence"".""TempTableIsMigratingCorrespondences"" (
+                    LIKE ""correspondence"".""Correspondences""
+                );
+            ");
+
+            migrationBuilder.Sql(@"
+                ALTER TABLE ""correspondence"".""TempTableIsMigratingCorrespondences""
+                    ADD PRIMARY KEY (""Id"");
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DROP TABLE IF EXISTS ""correspondence"".""TempTableIsMigratingCorrespondences"";
+            ");
+        }
+    }
+}
