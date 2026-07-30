868da21cc1aa6715ee4c91caf64eb930a55b7d26
Magnus Åsheim <magnus.hansen.asheim@digdir.no>
2026-07-30 09:23:20 +0200

    increase expiration time

--- src/Altinn.Correspondence.Persistence/Repositories/StorageRepository.cs
+++ src/Altinn.Correspondence.Persistence/Repositories/StorageRepository.cs
@@ -417,7 +417,7 @@ namespace Altinn.Correspondence.Persistence.Repositories
         BlockBlobClient blockBlobClient = blobContainerClient.GetBlockBlobClient(attachment.Id.ToString());
 
         var startsOn = DateTimeOffset.UtcNow.AddMinutes(-5);
-        var expiresOn = DateTimeOffset.UtcNow.AddMinutes(15);
+        var expiresOn = DateTimeOffset.UtcNow.AddMinutes(30);
 
         // Generate a SAS token that is valid for 1 hour
         var sasBuilder = new Azure.Storage.Sas.BlobSasBuilder
