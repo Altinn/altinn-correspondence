6dc244453f207890188f5c96d58e4bc6f64965ba
Ceredron <roar.mjelde@digdir.no>
2026-08-31 11:59:49 +0200

    Revert "Set confidential in load tests"
    This reverts commit 9415106ec3563a14bf5212860138c9c2d99b2046.

--- Test/Altinn.Correspondence.LoadTests/test_initialize_correspondence.js
+++ Test/Altinn.Correspondence.LoadTests/test_initialize_correspondence.js
@@ -57,7 +57,6 @@ const data_without_attachment = {
             ReminderSmsBody: "Dette er en test av revarslingl",
         },
         isReservable: true,
-        isConfidential: true
     },
     Recipients: ["0192:123456789"],
     existingAttachments: [],
